b54bf2c8bb6f8d7d8c37bdfe62dec776200d9fcf
mark <mark@a9ff2418-eebe-19e0-0898-3207744d8c45>
2009-10-27 04:32:03 +0000

    Resolve: Damage can potentially modify the collection during the enumeration.

--- Scripts/Items/Misc/AcidSlime.cs
+++ Scripts/Items/Misc/AcidSlime.cs
@@ -47,23 +47,28 @@ namespace Server.Items
 			DateTime now = DateTime.Now;
 			TimeSpan age = now - m_Created;
 
-			if( age > m_Duration )
+			if( age > m_Duration ) {
 				Delete();
-			else
-			{
+			} else {
 				if( !m_Drying && age > (m_Duration - age) )
 				{
 					m_Drying = true;
 					ItemID = 0x122B;
 				}
+
+				List<Mobile> toDamage = new List<Mobile>();
+
 				foreach( Mobile m in GetMobilesInRange( 0 ) )
 				{
 					BaseCreature bc = m as BaseCreature;
 					if( m.Alive && !m.IsDeadBondedPet && (bc == null || bc.Controlled || bc.Summoned) )
 					{
-						Damage ( m );
+						toDamage.Add( m );
 					}
 				}
+
+				for ( int i = 0; i < toDamage.Count; i++ )
+					Damage( toDamage[i] );
 			}
 		}
 
