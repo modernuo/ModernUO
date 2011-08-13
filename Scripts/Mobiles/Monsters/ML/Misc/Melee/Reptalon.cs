e39b6950a4b5838c496c68c9594349d54bafbe6d
xavier <xavier@a9ff2418-eebe-19e0-0898-3207744d8c45>
2011-08-13 15:21:45 +0000

    .1 active speed?

--- Scripts/Mobiles/Monsters/ML/Misc/Melee/Reptalon.cs
+++ Scripts/Mobiles/Monsters/ML/Misc/Melee/Reptalon.cs
@@ -8,7 +8,7 @@ namespace Server.Mobiles
 	public class Reptalon : BaseMount
 	{
 		[Constructable]
-		public Reptalon() : base( "a reptalon", 0x114, 0x3E90, AIType.AI_Melee, FightMode.Closest, 10, 1, 0.1, 0.2 )
+		public Reptalon() : base( "a reptalon", 0x114, 0x3E90, AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.35 )
 		{
 			BaseSoundID = 0x16A; // TODO check
 
