7b48ebb7e6e53bb6ff23119975b4437125d0a3f3
asayre <asayre@a9ff2418-eebe-19e0-0898-3207744d8c45>
2012-03-07 10:08:55 +0000

    Implemented ScreenEffects.

--- Server/Network/Packets.cs
+++ Server/Network/Packets.cs
@@ -1526,6 +1526,77 @@ namespace Server.Network
 		}
 	}
 
+	public enum ScreenEffectType
+	{
+		FadeOut = 0x00,
+		FadeIn = 0x01,
+		LightFlash = 0x02,
+		FadeInOut = 0x03,
+		DarkFlash = 0x04
+	}
+
+	public class ScreenEffect : Packet
+	{
+		public ScreenEffect( ScreenEffectType type )
+			: base( 0x70, 28 )
+		{
+			m_Stream.Write( (byte)0x04 );
+			m_Stream.Fill( 8 );
+			m_Stream.Write( (short)type );
+			m_Stream.Fill( 16 );
+		}
+	}
+
+	public sealed class ScreenFadeOut : ScreenEffect
+	{
+		public static readonly Packet Instance = Packet.SetStatic( new ScreenFadeOut() );
+
+		public ScreenFadeOut()
+			: base( ScreenEffectType.FadeOut )
+		{ 
+		}
+	}
+
+	public sealed class ScreenFadeIn : ScreenEffect
+	{
+		public static readonly Packet Instance = Packet.SetStatic( new ScreenFadeIn() );
+
+		public ScreenFadeIn()
+			: base( ScreenEffectType.FadeIn )
+		{
+		}
+	}
+
+	public sealed class ScreenFadeInOut : ScreenEffect
+	{
+		public static readonly Packet Instance = Packet.SetStatic( new ScreenFadeInOut() );
+
+		public ScreenFadeInOut()
+			: base( ScreenEffectType.FadeInOut )
+		{ 
+		}
+	}
+
+	public sealed class ScreenLightFlash : ScreenEffect
+	{
+		public static readonly Packet Instance = Packet.SetStatic( new ScreenLightFlash() );
+
+		public ScreenLightFlash()
+			: base( ScreenEffectType.LightFlash )
+		{
+		}
+	}
+
+	public sealed class ScreenDarkFlash : ScreenEffect
+	{
+		public static readonly Packet Instance = Packet.SetStatic( new ScreenDarkFlash() );
+
+		public ScreenDarkFlash()
+			: base( ScreenEffectType.DarkFlash )
+		{ 
+		}
+	}
+
 	public enum DeleteResultType
 	{
 		PasswordInvalid,
