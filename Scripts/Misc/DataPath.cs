6d6866ea17e67d16b5fbd57ecf98d19ebeef3594
mark <mark@a9ff2418-eebe-19e0-0898-3207744d8c45>
2012-11-25 06:03:20 +0000

    look for additional registry keys

--- Scripts/Misc/DataPath.cs
+++ Scripts/Misc/DataPath.cs
@@ -37,6 +37,7 @@ namespace Server.Misc
 			string pathTD = GetPath( @"Origin Worlds Online\Ultima Online Third Dawn\1.0", "ExePath" ); //These refer to 2D & 3D, not the Third Dawn expansion
 			string pathKR = GetPath( @"Origin Worlds Online\Ultima Online\KR Legacy Beta", "ExePath" ); //After KR, This is the new registry key for the 2D client
 			string pathSA = GetPath( @"Electronic Arts\EA Games\Ultima Online Stygian Abyss Classic", "InstallDir" );
+			string pathHS = GetPath( @"Electronic Arts\EA Games\Ultima Online Classic", "InstallDir" );
 
 			if ( CustomPath != null ) 
 				Core.DataDirectories.Add( CustomPath ); 
@@ -52,6 +53,9 @@ namespace Server.Misc
 
 			if ( pathSA != null )
 				Core.DataDirectories.Add( pathSA );
+
+			if ( pathHS != null )
+				Core.DataDirectories.Add( pathHS );
 
 			if ( Core.DataDirectories.Count == 0 && !Core.Service )
 			{
