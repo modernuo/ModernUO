c7e36397d30b78de44ddc371bf40c5afcc5de3e3
xavier <xavier@a9ff2418-eebe-19e0-0898-3207744d8c45>
2012-12-23 06:39:20 +0000

    oops

--- Scripts/Items/Skill Items/Tailor Items/Dyetubs/LeatherDyeTub.cs
+++ Scripts/Items/Skill Items/Tailor Items/Dyetubs/LeatherDyeTub.cs
@@ -9,7 +9,7 @@ namespace Server.Items
 		public override int TargetMessage{ get{ return 1042416; } } // Select the leather item to dye.
 		public override int FailMessage{ get{ return 1042418; } } // You can only dye leather with this tub.
 		public override int LabelNumber{ get{ return 1041284; } } // Leather Dye Tub
-		public override CustomHuePicker CustomHuePicker{ get{ return null; } }
+		public override CustomHuePicker CustomHuePicker { get { return CustomHuePicker.LeatherDyeTub; } }
 
 		private bool m_IsRewardItem;
 
