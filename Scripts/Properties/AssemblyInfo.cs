abe9f5acb212310c6a3f9c8b8eb315f05a9928cc
Mark Sturgill <msturgill@gmail.com>
2014-02-25 13:56:48 -0700

    add AssemblyInfo.cs to Scripts (primarily to remove ComVisible code analyzer nag)

---
+++ Scripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RunUO Scripts Assembly")]
+[assembly: AssemblyDescription("RunUO Scripts Assembly")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RunUO")]
+[assembly: AssemblyProduct("RunUO")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07fe114c-a456-42f8-82fd-251c9b78cd49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
