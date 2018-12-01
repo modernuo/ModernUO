46dbb932ffd8f69cd00fb68b383b6afd5eb7d683
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2018-12-01 00:19:35 -0800

    Fixes props gump

--- Scripts/Misc/VendorGenerator.cs
+++ Scripts/Misc/VendorGenerator.cs
@@ -132,6 +132,8 @@ namespace Server
           for (int y = 0; y < map.Height; ++y)
             CheckPoint(map, regions[i].X + x, regions[i].Y + y);
 
+      Console.WriteLine("We checked points");
+
       for (int i = 0; i < m_ShopList.Count; ++i)
       {
         ShopInfo si = m_ShopList[i];
@@ -281,62 +283,22 @@ namespace Server
 
     private static bool IsArmor(int itemID)
     {
-      if (itemID >= 0x13BB && itemID <= 0x13E2)
-        return true;
-
-      if (itemID >= 0x13E5 && itemID <= 0x13F2)
-        return true;
-
-      if (itemID >= 0x1408 && itemID <= 0x141A)
-        return true;
-
-      if (itemID >= 0x144E && itemID <= 0x1457)
-        return true;
-
-      return false;
+      return itemID >= 0x13BB && itemID <= 0x13E2 || itemID >= 0x13E5 && itemID <= 0x13F2 ||
+             itemID >= 0x1408 && itemID <= 0x141A || itemID >= 0x144E && itemID <= 0x1457;
     }
 
     private static bool IsMetalWeapon(int itemID)
     {
-      if (itemID >= 0xF43 && itemID <= 0xF4E)
-        return true;
-
-      if (itemID >= 0xF51 && itemID <= 0xF52)
-        return true;
-
-      if (itemID >= 0xF5C && itemID <= 0xF63)
-        return true;
-
-      if (itemID >= 0x13AF && itemID <= 0x13B0)
-        return true;
-
-      if (itemID >= 0x13B5 && itemID <= 0x13BA)
-        return true;
-
-      if (itemID >= 0x13FA && itemID <= 0x13FB)
-        return true;
-
-      if (itemID >= 0x13FE && itemID <= 0x1407)
-        return true;
-
-      if (itemID >= 0x1438 && itemID <= 0x1443)
-        return true;
-
-      return false;
+      return itemID >= 0xF43 && itemID <= 0xF4E || itemID >= 0xF51 && itemID <= 0xF52 ||
+             itemID >= 0xF5C && itemID <= 0xF63 || itemID >= 0x13AF && itemID <= 0x13B0 ||
+             itemID >= 0x13B5 && itemID <= 0x13BA || itemID >= 0x13FA && itemID <= 0x13FB ||
+             itemID >= 0x13FE && itemID <= 0x1407 || itemID >= 0x1438 && itemID <= 0x1443;
     }
 
     private static bool IsArcheryWeapon(int itemID)
     {
-      if (itemID >= 0xF4F && itemID <= 0xF50)
-        return true;
-
-      if (itemID >= 0x13B1 && itemID <= 0x13B2)
-        return true;
-
-      if (itemID >= 0x13FC && itemID <= 0x13FD)
-        return true;
-
-      return false;
+      return itemID >= 0xF4F && itemID <= 0xF50 || itemID >= 0x13B1 && itemID <= 0x13B2 ||
+             itemID >= 0x13FC && itemID <= 0x13FD;
     }
 
     private static ShopFlags ProcessDisplayedItem(int itemID)
@@ -481,9 +443,9 @@ namespace Server
       floor.Add(p);
 
       for (int xo = -1; xo <= 1; ++xo)
-      for (int yo = -1; yo <= 1; ++yo)
-        if ((xo != 0 || yo != 0) && IsFloor(map, x + xo, y + yo, false))
-          RecurseFindFloor(map, x + xo, y + yo, floor);
+        for (int yo = -1; yo <= 1; ++yo)
+          if ((xo != 0 || yo != 0) && IsFloor(map, x + xo, y + yo, false))
+            RecurseFindFloor(map, x + xo, y + yo, floor);
     }
 
     [Flags]
