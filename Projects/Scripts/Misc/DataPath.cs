ee0dd2b0fa2c899067254cd4f02781f05c7769b4
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2019-08-02 16:42:10 -0700

    Reorganizes the entire project.

--- Projects/Scripts/Misc/DataPath.cs
+++ Projects/Scripts/Misc/DataPath.cs
@@ -10,8 +10,8 @@ namespace Server.Misc
      * Example:
      *  private static string CustomPath = @"C:\Program Files\Ultima Online";
      */
-    private static string CustomPath = @"C:\Ultima Online Classic";
-    // private static string CustomPath = @"/Users/kamronbatman/UOC";
+    // private static string CustomPath = @"C:\Ultima Online Classic";
+    private static string CustomPath = @"/Users/kamronbatman/UOC";
 
     /* The following is a list of files which a required for proper execution:
      *
