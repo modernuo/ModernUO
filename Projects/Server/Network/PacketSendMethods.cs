a3602b937bc02c3a3e74ba88a5f2efac1bd9a777
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2019-08-08 15:44:23 -0700

    Forgot the send command

--- Projects/Server/Network/PacketSendMethods.cs
+++ Projects/Server/Network/PacketSendMethods.cs
@@ -30,6 +30,7 @@ namespace Server.Network
   public delegate WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8> FixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8>(out int length);
   public delegate void WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8>(Memory<byte> mem, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8);
 
+  // For Moving Effects
   public delegate WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9> FixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9>(out int length);
   public delegate void WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Memory<byte> mem, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9);
 
@@ -72,12 +73,6 @@ namespace Server.Network
 
   public static partial class Packets
   {
-    public static void TestSend(NetState ns)
-    {
-      Send(ns, SetArrowHS, (short)1, (short)1, Serial.MinusOne);
-      Send(ns, DisplaySecureTrade, Serial.MinusOne, Serial.MinusOne, Serial.MinusOne, "name");
-    }
-
     #region Send Fixed Packet Methods
     public static void Send(NetState ns, FixedPacketMethod f)
     {
@@ -151,11 +146,22 @@ namespace Server.Network
       _ = ns.Flush(length);
     }
 
+    // For MovingEffects
     public static void Send<T1, T2, T3, T4, T5, T6, T7, T8, T9>(NetState ns, FixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9> f, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9)
     {
       WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9> func = f(out int length);
       Memory<byte> mem = ns.SendPipe.Writer.GetMemory(length);
       func(mem, t1, t2, t3, t4, t5, t6, t7, t8, t9);
+      _ = ns.Flush(length);
+    }
+
+    // For MovingParticleEffects
+    public static void Send<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(NetState ns, FixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> f,
+      T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12, T13 t13, T14 t14)
+    {
+      WriteFixedPacketMethod<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> func = f(out int length);
+      Memory<byte> mem = ns.SendPipe.Writer.GetMemory(length);
+      func(mem, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14);
       _ = ns.Flush(length);
     }
     #endregion
