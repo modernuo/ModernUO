8cf8bc4dee68526c8a80eb607738b3495d3b34bc
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2019-09-16 00:52:48 -0700

    Finishes mahjon packets

--- Projects/Scripts/Items/Games/Mahjong/Packets.cs
+++ Projects/Scripts/Items/Games/Mahjong/Packets.cs
@@ -127,20 +127,25 @@ namespace Server.Engines.Mahjong
 
     public static void SendMahjongTilesInfo(NetState ns, MahjongGame game, Mobile to)
     {
+      if (ns == null)
+        return;
+
       MahjongTile[] tiles = game.Tiles;
       MahjongPlayers players = game.Players;
 
-      EnsureCapacity(11 + 9 * tiles.Length);
+      int length = 11 + 9 * tiles.Length;
+      SpanWriter writer = new SpanWriter(stackalloc byte[length]);
+      writer.Write((byte)0xDA); // Packet ID
+      writer.Write((short)length); // Dynamic Length
 
-      m_Stream.Write(game.Serial);
-      m_Stream.Write((byte)0);
-      m_Stream.Write((byte)0x4);
+      writer.Write(game.Serial);
+      writer.Write((short)0x4);
 
-      m_Stream.Write((short)tiles.Length);
+      writer.Write((short)tiles.Length);
 
       foreach (MahjongTile tile in tiles)
       {
-        m_Stream.Write((byte)tile.Number);
+        writer.Write((byte)tile.Number);
 
         if (tile.Flipped)
         {
@@ -148,39 +153,40 @@ namespace Server.Engines.Mahjong
 
           if (hand < 0 || players.IsPublic(hand) || players.GetPlayer(hand) == to ||
               game.SpectatorVision && players.IsSpectator(to))
-            m_Stream.Write((byte)tile.Value);
+            writer.Write((byte)tile.Value);
           else
-            m_Stream.Write((byte)0);
+            writer.Position++;
         }
         else
-        {
-          m_Stream.Write((byte)0);
-        }
+          writer.Position++;
 
-        m_Stream.Write((short)tile.Position.Y);
-        m_Stream.Write((short)tile.Position.X);
-        m_Stream.Write((byte)tile.StackLevel);
-        m_Stream.Write((byte)tile.Direction);
+        writer.Write((short)tile.Position.Y);
+        writer.Write((short)tile.Position.X);
+        writer.Write((byte)tile.StackLevel);
+        writer.Write((byte)tile.Direction);
 
-        m_Stream.Write(tile.Flipped ? (byte)0x10 : (byte)0x0);
+        writer.Write(tile.Flipped ? (byte)0x10 : (byte)0x0);
       }
-    }
-  }
 
-  public sealed class MahjongTileInfo : Packet
-  {
-    public MahjongTileInfo(MahjongTile tile, Mobile to) : base(0xDA)
+      ns.Send(writer.Span);
+    }
+
+    public static void SendMahjongTileInfo(NetState ns, MahjongTile tile, Mobile to)
     {
+      if (ns == null)
+        return;
+
       MahjongGame game = tile.Game;
       MahjongPlayers players = game.Players;
 
-      EnsureCapacity(18);
+      SpanWriter writer = new SpanWriter(stackalloc byte[18]);
+      writer.Write((byte)0xDA); // Packet ID
+      writer.Write((short)18); // Dynamic Length
 
-      m_Stream.Write(tile.Game.Serial);
-      m_Stream.Write((byte)0);
-      m_Stream.Write((byte)0x3);
+      writer.Write(game.Serial);
+      writer.Write((short)0x3);
 
-      m_Stream.Write((byte)tile.Number);
+      writer.Write((byte)tile.Number);
 
       if (tile.Flipped)
       {
@@ -188,33 +194,36 @@ namespace Server.Engines.Mahjong
 
         if (hand < 0 || players.IsPublic(hand) || players.GetPlayer(hand) == to ||
             game.SpectatorVision && players.IsSpectator(to))
-          m_Stream.Write((byte)tile.Value);
+          writer.Write((byte)tile.Value);
         else
-          m_Stream.Write((byte)0);
+          writer.Position++;
       }
       else
-      {
-        m_Stream.Write((byte)0);
-      }
+        writer.Position++;
 
-      m_Stream.Write((short)tile.Position.Y);
-      m_Stream.Write((short)tile.Position.X);
-      m_Stream.Write((byte)tile.StackLevel);
-      m_Stream.Write((byte)tile.Direction);
+      writer.Write((short)tile.Position.Y);
+      writer.Write((short)tile.Position.X);
+      writer.Write((byte)tile.StackLevel);
+      writer.Write((byte)tile.Direction);
 
-      m_Stream.Write(tile.Flipped ? (byte)0x10 : (byte)0x0);
+      writer.Write(tile.Flipped ? (byte)0x10 : (byte)0x0);
+
+      ns.Send(writer.Span);
     }
-  }
 
-  public sealed class MahjongRelieve : Packet
-  {
-    public MahjongRelieve(MahjongGame game) : base(0xDA)
+    public static void SendMahjongRelieve(NetState ns, MahjongGame game)
     {
-      EnsureCapacity(9);
+      if (ns == null)
+        return;
 
-      m_Stream.Write(game.Serial);
-      m_Stream.Write((byte)0);
-      m_Stream.Write((byte)0x1A);
+      SpanWriter writer = new SpanWriter(stackalloc byte[18]);
+      writer.Write((byte)0xDA); // Packet ID
+      writer.Write((short)18); // Dynamic Length
+
+      writer.Write(game.Serial);
+      writer.Write((short)0x1A);
+
+      ns.Send(writer.Span);
     }
   }
 }
