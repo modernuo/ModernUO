031b3656c6e5a89ee20bcd297b58218c40ec37e6
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2019-11-24 15:20:48 -0800

    WIP

---
+++ Projects/Server/LibUv/LibuvTransportContext.cs
@@ -0,0 +1,16 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Hosting;
+
+namespace Libuv
+{
+  public class LibuvTransportContext
+  {
+    public LibuvTransportOptions Options { get; set; }
+
+    public IHostApplicationLifetime AppLifetime { get; set; }
+
+    public ILibuvTrace Log { get; set; }
+  }
+}
