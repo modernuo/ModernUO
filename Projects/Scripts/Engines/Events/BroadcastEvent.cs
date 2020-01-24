680bbf8786ba7889ab2651a32d33a73ca9825d8d
Leath Cooper <leathcooper@gmail.com>
2020-01-24 00:05:50 -0500

    basic event scheduler

---
+++ Projects/Scripts/Engines/Events/BroadcastEvent.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Engines.Events
+{
+  public class BroadcastEvent : IEvent
+  {
+    private int _hue = 0;
+    private string _text = "";
+    public static void Initialize()
+    {
+      /*
+      EventScheduler.Instance.ScheduleEvent(new BroadcastEvent(22, "Test Message Please Ignore 2min"), 0, 0, TimeSpan.FromMinutes(2.0));
+      EventScheduler.Instance.ScheduleEvent(new BroadcastEvent(33, "Test Message Please Ignore 3min"), 0, 0, TimeSpan.FromMinutes(3.0));
+      EventScheduler.Instance.ScheduleEvent(new BroadcastEvent(44, "Test Message Please Ignore 4min"), 0, 0, TimeSpan.FromMinutes(4.0));
+      */
+    }
+
+    public BroadcastEvent(int hue, string text)
+    {
+      _hue = hue;
+      _text = text;
+    }
+
+    public override string ToString()
+    {
+      return $"Broadcast: {_text}";
+    }
+
+    public void OnEventScheduled()
+    {
+      World.Broadcast(_hue, true, _text);
+    }
+  }
+}
