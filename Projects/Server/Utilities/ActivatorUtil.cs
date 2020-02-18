269c1c2f1f18741a825a3bad7895530541cd6b04
Andrew Fryer <flamewave000@live.com>
2020-02-18 11:46:51 -0500

    improvements and expansion on the ActivatorUtil

--- Projects/Server/Utilities/ActivatorUtil.cs
+++ Projects/Server/Utilities/ActivatorUtil.cs
@@ -1,51 +1,89 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace Server.Utilities
 {
   public static class ActivatorUtil
   {
-    public static object CreateInstance(Type type)
+
+    public static ConstructorInfo GetConstructor(Type type)
     {
       try
       {
-        if (type.GetConstructor(Type.EmptyTypes) != null)
-          return Activator.CreateInstance(type);
-        else
-        {
-          var emptyConstructor = type.GetConstructors().Single(info =>
-          {
-            var paramList = info.GetParameters();
-            return paramList.Length == 1 && paramList[0].IsOptional;
-          });
-          if (emptyConstructor != null)
-            return emptyConstructor.Invoke(new object[] { Type.Missing });
-          else
-            throw new TypeInitializationException(type.ToString(), new Exception($"There is no empty/default constructor for {type}"));
-        }
+        return type.GetConstructor(Type.EmptyTypes)
+          ?? type.GetConstructors().Single(info => info.GetParameters().All(x => x.IsOptional))
+          ?? throw new TypeInitializationException(type.ToString(), new Exception($"There is no empty/default constructor for {type}"));
       }
-      catch (NullReferenceException e)
+      catch (Exception e)
       {
-        throw new TypeInitializationException(type.ToString(), new Exception($"There is no empty/default constructor for {type}", e));
+        throw new TypeInitializationException(type.ToString(), e);
+      }
+    }
+    public static ConstructorInfo GetConstructor(Type type, params Type[] args)
+    {
+      try
+      {
+        ConstructorInfo cctor = null;
+        if (args.All(x => x != null))
+          cctor = type.GetConstructor(args);
+        if (cctor == null)
+          cctor = type.GetConstructors().Single(info =>
+          {
+            var paramList = info.GetParameters().ToList();
+            // If more args are given than parameters, skip.
+            if (args.Length > paramList.Count)
+              return false;
+            // check all given args map to params.
+            for (int i = 0; i < args.Length; i++)
+            {
+              // if a null reference is passed, but the type is not nullable, skip.
+              if (args[i] == null)
+              {
+                if (paramList[i].ParameterType.IsValueType) return false;
+              }
+              // If an arg is not assignable to the parameter type, skip.
+              else if (!paramList[i].ParameterType.IsAssignableFrom(args[i]))
+                return false;
+            }
+            // If there are more parameters, check if they any are not optional, if any are not, skip.
+            if (args.Length < paramList.Count && paramList.GetRange(args.Length, paramList.Count - args.Length).Any(x => !x.IsOptional))
+              return false;
+            // All checks have passed. We have found a match.
+            return true;
+          });
+        if (cctor == null)
+          throw new TypeInitializationException(type.ToString(), new Exception($"There is no empty/default constructor for {type}"));
+        else
+          return cctor;
+      }
+      catch (Exception e)
+      {
+        throw new TypeInitializationException(type.ToString(), e);
+      }
+    }
+    public static object CreateInstance(Type type)
+    {
+      var cctor = GetConstructor(type);
+      var args = cctor.GetParameters();
+      if (args.Length == 0)
+        return cctor.Invoke(Type.EmptyTypes);
+      else
+      {
+        var argList = new object[args.Length];
+        Array.Fill(argList, Type.Missing);
+        return cctor.Invoke(argList);
       }
     }
     public static object CreateInstance(Type type, params object[] args)
     {
       if (args == null || args.Length == 0)
         return CreateInstance(type);
-      try
-      {
-        return Activator.CreateInstance(type, args);
-      }
-      catch (NullReferenceException e)
-      {
-        throw new TypeInitializationException(type.ToString(), new Exception($"There is no constructor for {type} with the given parameter types", e));
-      }
-      throw new TypeInitializationException(type.ToString(), new Exception($"There is no constructor for {type} with the given parameter types"));
+      var cctor = GetConstructor(type, args.Select(x => x?.GetType()).ToArray());
+      return cctor.Invoke(args);
     }
 
     public static T CreateInstance<T>() => (T)CreateInstance(typeof(T));
+    public static T CreateInstance<T>(params object[] args) => (T)CreateInstance(typeof(T), args);
   }
 }
