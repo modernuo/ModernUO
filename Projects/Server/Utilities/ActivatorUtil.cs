11cac69eba52442f150f4ce264ecff4754e1bab3
Reetus <6239195+Reetus@users.noreply.github.com>
2020-02-20 18:08:01 +0800

    Spawner wouldn't spawn Types with optional parameters in ctor due to parameter length check (#92)

--- Projects/Server/Utilities/ActivatorUtil.cs
+++ Projects/Server/Utilities/ActivatorUtil.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,68 +7,120 @@ namespace Server.Utilities
 {
   public static class ActivatorUtil
   {
+    public static ConstructorInfo GetConstructor(Type type, Predicate<ConstructorInfo> predicate = null)
+    {
+      ConstructorInfo emptyCtor = type.GetConstructor(Type.EmptyTypes);
 
-    public static ConstructorInfo GetConstructor(Type type)
-    {
-      try
+      if (emptyCtor != null && predicate?.Invoke(emptyCtor) != false)
       {
-        return type.GetConstructor(Type.EmptyTypes)
-          ?? type.GetConstructors().Single(info => info.GetParameters().All(x => x.IsOptional))
-          ?? throw new TypeInitializationException(type.ToString(), new Exception($"There is no empty/default constructor for {type}"));
+        return emptyCtor;
       }
-      catch (Exception e)
+
+      ConstructorInfo optionalCtor = type.GetConstructors().SingleOrDefault(info =>
+        predicate?.Invoke(info) != false && info.GetParameters().All(x => x.IsOptional));
+
+      if (optionalCtor != null)
       {
-        throw new TypeInitializationException(type.ToString(), e);
+        return optionalCtor;
       }
+
+      throw new TypeInitializationException(type.ToString(),
+        new Exception($"There is no empty/default constructor for {type} that matches predicate."));
     }
-    public static ConstructorInfo GetConstructor(Type type, params Type[] args)
+
+    public static ConstructorInfo GetConstructor(Type type, Predicate<ConstructorInfo> predicate, params Type[] args)
     {
       try
       {
-        return args.All(x => x != null) ? type.GetConstructor(args) : null ?? type.GetConstructors().Single(info =>
+        ConstructorInfo ctor;
+
+        if (args.All(x => x != null))
+        {
+          ctor = type.GetConstructor(args);
+
+          if (ctor != null && predicate?.Invoke(ctor) != false)
           {
-            var paramList = info.GetParameters().ToList();
+            return ctor;
+          }
+        }
+        else
+        {
+          ctor = type.GetConstructors().SingleOrDefault(info =>
+          {
+            if (predicate?.Invoke(info) == false)
+            {
+              return false;
+            }
+
+            List<ParameterInfo> paramList = info.GetParameters().ToList();
+
             // If more args are given than parameters, skip.
             if (args.Length > paramList.Count)
+            {
               return false;
+            }
+
             // check all given args map to params.
             for (int i = 0; i < args.Length; i++)
             {
               // if a null reference is passed, but the type is not nullable
-              if ((args[i] == null && paramList[i].ParameterType.IsValueType)
-              // or if an arg is not null and is not assignable to the parameter type, skip.
-                || !(args[i] == null || paramList[i].ParameterType.IsAssignableFrom(args[i])))
+              if (args[i] == null && paramList[i].ParameterType.IsValueType
+                  // or if an arg is not null and is not assignable to the parameter type, skip.
+                  || !(args[i] == null || paramList[i].ParameterType.IsAssignableFrom(args[i])))
+              {
                 return false;
+              }
             }
+
             // If there are more parameters, check if they any are not optional, if any are not, skip.
             // Otherwise all checks have passed. We have found a match 
-            return args.Length <= paramList.Count || paramList.GetRange(args.Length, paramList.Count - args.Length).All(x => x.IsOptional);
-          }) ?? throw new Exception($"There is no empty/default constructor for {type}");
+            return args.Length <= paramList.Count || paramList.GetRange(args.Length, paramList.Count - args.Length)
+                     .All(x => x.IsOptional);
+          });
+          
+          if (ctor != null)
+          {
+            return ctor;
+          }
+        }
+
+        throw new Exception($"There is no empty/default constructor for {type} that matches predicate.");
       }
       catch (Exception e)
       {
         throw new TypeInitializationException(type.ToString(), e);
       }
     }
-    public static object CreateInstance(Type type)
+
+    public static object CreateInstance(Type type, Predicate<ConstructorInfo> constructorPredicate = null)
     {
-      var cctor = GetConstructor(type);
-      var args = cctor.GetParameters();
+      ConstructorInfo cctor = GetConstructor(type, constructorPredicate);
+      ParameterInfo[] args = cctor.GetParameters();
+
       if (args.Length == 0)
+      {
         return cctor.Invoke(Type.EmptyTypes);
-      var argList = new object[args.Length];
+      }
+
+      object[] argList = new object[args.Length];
       Array.Fill(argList, Type.Missing);
       return cctor.Invoke(argList);
     }
-    public static object CreateInstance(Type type, params object[] args)
+
+    public static object CreateInstance(Type type, Predicate<ConstructorInfo> constructorPredicate = null,
+      params object[] args)
     {
       if (args == null || args.Length == 0)
-        return CreateInstance(type);
-      var cctor = GetConstructor(type, args.Select(x => x?.GetType()).ToArray());
+      {
+        return CreateInstance(type, constructorPredicate);
+      }
+
+      ConstructorInfo cctor = GetConstructor(type, constructorPredicate, args.Select(x => x?.GetType()).ToArray());
       return cctor.Invoke(args);
     }
 
-    public static T CreateInstance<T>() => (T)CreateInstance(typeof(T));
-    public static T CreateInstance<T>(params object[] args) => (T)CreateInstance(typeof(T), args);
+    public static object CreateInstance(Type type, params object[] args) => CreateInstance(type, null, args);
+    public static T CreateInstance<T>(Predicate<ConstructorInfo> constructorPredicate = null) => (T)CreateInstance(typeof(T), constructorPredicate);
+    public static T CreateInstance<T>(params object[] args) => (T)CreateInstance(typeof(T), null, args);
   }
 }
