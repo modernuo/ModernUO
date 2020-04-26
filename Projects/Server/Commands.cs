bc117e1666e297ea91437f0c9911d05f3672567b
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-04-26 00:08:02 -0700

    Reverting

--- Projects/Server/Commands.cs
+++ Projects/Server/Commands.cs
@@ -117,22 +117,6 @@ namespace Server
     public AccessLevel AccessLevel { get; }
 
     public int CompareTo(CommandEntry e) => e == null ? 1 : Command.CompareTo(e.Command);
-
-    public override bool Equals(object obj) => obj is CommandEntry e && CompareTo(e) == 0;
-
-    public override int GetHashCode() => Command.GetHashCode();
-
-    public static bool operator ==(CommandEntry left, CommandEntry right) => left.CompareTo(right) == 0;
-
-    public static bool operator !=(CommandEntry left, CommandEntry right) => left.CompareTo(right) != 0;
-
-    public static bool operator <(CommandEntry left, CommandEntry right) => left.CompareTo(right) < 0;
-
-    public static bool operator <=(CommandEntry left, CommandEntry right) => left.CompareTo(right) <= 0;
-
-    public static bool operator >(CommandEntry left, CommandEntry right) => left.CompareTo(right) > 0;
-
-    public static bool operator >=(CommandEntry left, CommandEntry right) => left.CompareTo(right) >= 0;
   }
 
   public static class CommandSystem
