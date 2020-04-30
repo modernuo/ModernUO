65f67cf6da53bece6fd2da43ad821d098cad27bf
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-04-30 00:25:05 -0700

    Adds PBKDF2 protection. Removes the old shit

---
+++ Projects/Scripts/Accounting/PBKDF2PasswordProtection.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Buffers.Binary;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Server.Accounting
+{
+  public class PBKDF2PasswordProtection : IPasswordProtection
+  {
+    public static PBKDF2PasswordProtection Instance = new PBKDF2PasswordProtection();
+
+    private const ushort m_MinIterations = 1024;
+    private const ushort m_MaxIterations = 1536;
+    private static readonly HashAlgorithmName m_Algorithm = HashAlgorithmName.SHA256;
+    private const int m_SaltSize = 8;
+    private const int m_HashSize = 32;
+    private const int m_OutputSize = 2 + m_SaltSize + m_HashSize;
+
+    public string EncryptPassword(string plainPassword)
+    {
+      Span<byte> output = stackalloc byte[m_OutputSize];
+      int iterations = Utility.RandomMinMax(m_MinIterations, m_MaxIterations);
+      BinaryPrimitives.WriteUInt16LittleEndian(output.Slice(0, 2), (ushort)iterations);
+
+      var rfc2898 = new Rfc2898DeriveBytes(plainPassword, m_SaltSize, iterations, m_Algorithm);
+      rfc2898.Salt.CopyTo(output.Slice(2, m_SaltSize));
+      rfc2898.GetBytes(m_HashSize).CopyTo(output.Slice(m_SaltSize + 2));
+
+      return Encoding.Unicode.GetString(output);
+    }
+
+    public bool ValidatePassword(string encryptedPassword, string plainPassword)
+    {
+      Span<byte> encryptedBytes = stackalloc byte[m_OutputSize];
+      Encoding.Unicode.GetBytes(encryptedPassword, encryptedBytes);
+
+      ushort iterations = BinaryPrimitives.ReadUInt16LittleEndian(encryptedBytes.Slice(0, 2));
+      Span<byte> salt = encryptedBytes.Slice(2, m_SaltSize);
+
+      ReadOnlySpan<byte> hash =
+        new Rfc2898DeriveBytes(plainPassword, salt.ToArray(), iterations, m_Algorithm).GetBytes(m_HashSize);
+
+      return hash.SequenceEqual(encryptedBytes.Slice(m_SaltSize + 2));
+    }
+  }
+}
