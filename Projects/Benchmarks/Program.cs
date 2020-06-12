103568dadc3ad179fe2ea42e8605c11cdfad583a
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-06-12 12:18:02 -0700

    Adds Benchmarks

---
+++ Projects/Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var summary = BenchmarkRunner.Run<BenchmarkFeatureFlags>();
+    }
+  }
+}
