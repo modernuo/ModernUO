c4314c39c5c489053910a08c88a3154c26ee85fa
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-07-11 00:58:58 -0700

    Adds object help response packets

---
+++ Projects/Server.Tests/Network/Packets/Old/Outgoing/ObjectHelpResponseTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Buffers;
+using Server.Network;
+using Xunit;
+
+namespace Server.Tests.Network.Packets
+{
+  public class ObjectHelpResponseTests
+  {
+    [Fact]
+    public void TestObjectHelpResponse()
+    {
+      Serial s = 0x100;
+      string text = "This is some testing text";
+
+      Span<byte> data = new ObjectHelpResponse(s, text).Compile();
+
+      int length = 9 + text.Length * 2;
+
+      Span<byte> expectedData = stackalloc byte[length];
+      int pos = 0;
+
+      expectedData.Write(ref pos, (byte)0xB7); // Packet ID
+      expectedData.Write(ref pos, (ushort)length); // Length
+      expectedData.Write(ref pos, s);
+      expectedData.WriteBigUni(ref pos, text);
+
+#if NO_LOCAL_INIT
+      expectedData.Write(ref pos, (ushort)0); // Terminator
+#endif
+
+      AssertThat.Equal(data, expectedData);
+    }
+  }
+}
