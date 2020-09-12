20a4a75e767e7aef28291e246981a1e5a78f4e6b
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-09-12 02:12:15 -0700

    Removes uoam vendors.

--- Projects/UOContent/Misc/WelcomeTimer.cs
+++ Projects/UOContent/Misc/WelcomeTimer.cs
@@ -7,31 +7,37 @@ namespace Server.Misc
     /// </summary>
     public class WelcomeTimer : Timer
     {
-        private static readonly string[] m_Messages = TestCenter.Enabled
-            ? new[]
-            {
-                "Welcome to this test shard.  You are able to customize your character's stats and skills at anytime to anything you wish.  To see the commands to do this just say 'help'.",
-                "You will find a bank check worth 1,000,000 gold in your bank!",
-                "A spellbook and a bag of reagents has been placed into your bank box.",
-                "Various tools have been placed into your bank.",
-                "Various raw materials like ingots, logs, feathers, hides, bottles, etc, have been placed into your bank.",
-                "5 unmarked recall runes, 5 Felucca moonstones and 5 Trammel moonstones have been placed into your bank box.",
-                "One of each level of treasure map has been placed in your bank box.",
-                "You will find 9000 silver pieces deposited into your bank box.  Spend it as you see fit and enjoy yourself!",
-                "You will find 9000 gold pieces deposited into your bank box.  Spend it as you see fit and enjoy yourself!",
-                "A bag of PowerScrolls has been placed in your bank box."
-            }
-            : new[]
-            {
-                // Yes, this message is a pathetic message, It's suggested that you change it.
-                "Welcome to this shard.",
-                "Please enjoy your stay."
-            };
+        private static string[] m_Messages;
 
         private readonly int m_Count;
 
         private readonly Mobile m_Mobile;
         private int m_State;
+
+        public static void Initialize()
+        {
+            m_Messages = TestCenter.Enabled
+                ? new[]
+                {
+                    $"Welcome to {ServerList.ServerName}.",
+                    "You are able to customize your character's stats and skills at anytime to anything you wish.",
+                    "To see the commands to do this just say 'help'.",
+                    "You will find a bank check worth 1,000,000 gold in your bank!",
+                    "A spellbook and a bag of reagents has been placed into your bank box.",
+                    "Various tools have been placed into your bank.",
+                    "Various raw materials like ingots, logs, feathers, hides, bottles, etc, have been placed into your bank.",
+                    "5 unmarked recall runes, 5 Felucca moonstones and 5 Trammel moonstones have been placed into your bank box.",
+                    "One of each level of treasure map has been placed in your bank box.",
+                    "You will find 9000 silver pieces deposited into your bank box. Spend it as you see fit and enjoy yourself!",
+                    "You will find 9000 gold pieces deposited into your bank box. Spend it as you see fit and enjoy yourself!",
+                    "A bag of PowerScrolls has been placed in your bank box."
+                }
+                : new[]
+                {
+                    $"Welcome to {ServerList.ServerName}.",
+                    "Please enjoy your stay."
+                };
+        }
 
         public WelcomeTimer(Mobile m) : this(m, m_Messages.Length)
         {
@@ -46,10 +52,12 @@ namespace Server.Misc
         protected override void OnTick()
         {
             if (m_State < m_Count)
+            {
                 m_Mobile.SendMessage(0x35, m_Messages[m_State++]);
+                return;
+            }
 
-            if (m_State == m_Count)
-                Stop();
+            Stop();
         }
     }
 }
