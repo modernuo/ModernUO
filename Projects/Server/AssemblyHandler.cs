2a9a46077841a5eb24cbcbb15f4ad85ce2d5c575
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-10-17 20:44:36 -0700

    Removes unused namespaces

--- Projects/Server/AssemblyHandler.cs
+++ Projects/Server/AssemblyHandler.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
 
 namespace Server
