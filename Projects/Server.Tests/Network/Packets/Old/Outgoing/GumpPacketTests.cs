9098a8f4fb0233b3098ffcf00e3b2efb098b4fb5
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2020-10-25 22:22:16 -0700

    Removes unused namespaces

--- Projects/Server.Tests/Network/Packets/Old/Outgoing/GumpPacketTests.cs
+++ Projects/Server.Tests/Network/Packets/Old/Outgoing/GumpPacketTests.cs
@@ -2,8 +2,6 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Threading;
 using Server.Gumps;
 using Server.Network;
 using Xunit;
--- Projects/Server.Tests/Network/Packets/Old/Outgoing/MobilePacketTests.cs
+++ Projects/Server.Tests/Network/Packets/Old/Outgoing/MobilePacketTests.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Buffers;
-using System.Net;
-using System.Threading;
 using Server.Network;
 using Xunit;
 
