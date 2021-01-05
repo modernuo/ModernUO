8d46dc0d16572e78328a2b97d0ed04347b5dcf81
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-01-05 01:22:24 -0800

    Deletes unused

--- Projects/Server/Buffers/SpanWriter.cs
+++ Projects/Server/Buffers/SpanWriter.cs
@@ -220,11 +220,6 @@ namespace System.Buffers
 
             value ??= string.Empty;
 
-            if (value.Length == 0)
-            {
-                return;
-            }
-
             var charLength = Math.Min(fixedLength > -1 ? fixedLength : value.Length, value.Length);
             var src = value.AsSpan(0, charLength);
 
