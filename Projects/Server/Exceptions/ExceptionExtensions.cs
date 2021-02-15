45b3f601f857d89362ab6a411f6cce2691251fde
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-02-14 17:04:51 -0800

    fix(core): Changes invalid gump response to write to network errors.

---
+++ Projects/Server/Exceptions/ExceptionExtensions.cs
@@ -0,0 +1,42 @@
+/*************************************************************************
+ * ModernUO                                                              *
+ * Copyright (C) 2019-2021 - ModernUO Development Team                   *
+ * Email: hi@modernuo.com                                                *
+ * File: ExceptionExtensions.cs                                          *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Server.Exceptions
+{
+    public static class ExceptionExtensions
+    {
+        public static Exception SetStackTrace(this Exception target, StackTrace stack) => _setStackTrace(target, stack);
+
+        private static readonly Func<Exception, StackTrace, Exception> _setStackTrace = _createStackTraceMethod();
+
+        private static Func<Exception, StackTrace, Exception> _createStackTraceMethod()
+        {
+            ParameterExpression target = Expression.Parameter(typeof(Exception));
+            ParameterExpression stack = Expression.Parameter(typeof(StackTrace));
+            Type traceFormatType = typeof(StackTrace).GetNestedType("TraceFormat", BindingFlags.NonPublic);
+            MethodInfo toString = typeof(StackTrace).GetMethod("ToString", BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { traceFormatType }, null);
+            object normalTraceFormat = Enum.GetValues(traceFormatType!).GetValue(0);
+            MethodCallExpression stackTraceString = Expression.Call(stack, toString!, Expression.Constant(normalTraceFormat, traceFormatType));
+            FieldInfo stackTraceStringField = typeof(Exception).GetField("_stackTraceString", BindingFlags.NonPublic | BindingFlags.Instance);
+            BinaryExpression assign = Expression.Assign(Expression.Field(target, stackTraceStringField!), stackTraceString);
+            return Expression.Lambda<Func<Exception, StackTrace, Exception>>(Expression.Block(assign, target), target, stack).Compile();
+        }
+    }
+}
