4a30ca8b34bb07e9a1a1a15e711679ed3daa3e55
Maciej <m.t.czubak@gmail.com>
2021-03-03 13:47:44 +0000

    Rename Attributes to TypeAttributes.cs for clarity

---
+++ Projects/UOContent/Utilities/TypeAttributes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using static Server.Types;
+
+namespace Server
+{
+    public static class Attributes
+    {
+        public static bool IsConstructible(ConstructorInfo ctor, AccessLevel accessLevel)
+        {
+            var attrs = ctor.GetCustomAttributes(OfConstructible, false);
+
+            return attrs.Length != 0 && accessLevel >= ((ConstructibleAttribute)attrs[0]).AccessLevel;
+        }
+
+        public static CommandPropertyAttribute GetCPA(PropertyInfo p)
+        {
+            var attrs = p.GetCustomAttributes(OfCPA, false);
+
+            if (attrs.Length == 0)
+            {
+                return null;
+            }
+
+            return attrs[0] as CommandPropertyAttribute;
+        }
+        public static bool HasAttribute(Type type, Type check, bool inherit) =>
+            type.GetCustomAttributes(check, inherit).Length > 0;
+    }
+}
