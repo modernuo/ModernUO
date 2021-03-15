eeb6d5ecfd8bed846021a96b282935fded2bcff3
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-03-15 12:00:10 -0700

    Adds CUO and changes back to MapUO

--- Projects/UOContent/Network/MapUO.cs
+++ Projects/UOContent/Network/MapUO.cs
@@ -1,3 +1,18 @@
+/*************************************************************************
+ * ModernUO                                                              *
+ * Copyright (C) 2019-2021 - ModernUO Development Team                   *
+ * Email: hi@modernuo.com                                                *
+ * File: MapUO.cs                                                        *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
 using System;
 using System.Buffers;
 using System.IO;
@@ -6,7 +21,7 @@ using Server.Guilds;
 
 namespace Server.Network
 {
-    public static class ProtocolExtensions
+    public static class MapUO
     {
         private static readonly PacketHandler[] _handlers = new PacketHandler[0x100];
 
