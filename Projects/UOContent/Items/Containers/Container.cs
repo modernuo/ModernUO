7469fce68b66e62325ff46c7cc7c4a8633fb913f
3HMonkey <wewantthejoker@gmail.com>
2021-04-03 13:59:36 +0200

    OgranizeMe Command implementation

--- Projects/UOContent/Items/Containers/Container.cs
+++ Projects/UOContent/Items/Containers/Container.cs
@@ -354,6 +354,35 @@ namespace Server.Items
         }
     }
 
+    public class OrganizePouch : BaseContainer
+    {
+        [Constructible]
+        public OrganizePouch() : base(0xE79)
+        {
+            Weight = 1.0;
+            Name = "Organization Pouch";
+        }
+
+        public OrganizePouch(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(IGenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // version
+        }
+
+        public override void Deserialize(IGenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+
     public abstract class BaseBagBall : BaseContainer, IDyable
     {
         public BaseBagBall(int itemID) : base(itemID) => Weight = 1.0;
