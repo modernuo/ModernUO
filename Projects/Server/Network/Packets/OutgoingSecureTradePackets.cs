ddbcd937b2c9eb797735ec24a5d0864dbdca07d7
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-04-16 11:04:52 -0700

    fix(networking): Standardizes SecureTrade packets

--- Projects/Server/Network/Packets/OutgoingSecureTradePackets.cs
+++ Projects/Server/Network/Packets/OutgoingSecureTradePackets.cs
@@ -61,9 +61,12 @@ namespace Server.Network
 
             var writer = new SpanWriter(stackalloc byte[8]);
             writer.Write((byte)0x6F); // Packet ID
-            writer.Write((ushort)8); // Length
+            writer.Write((ushort)17); // Length
             writer.Write((byte)TradeFlag.Close);
             writer.Write(cont.Serial);
+            writer.Write(0);
+            writer.Write(0);
+            writer.Write(false);
 
             ns.Send(writer.Span);
         }
@@ -80,11 +83,12 @@ namespace Server.Network
 
             var writer = new SpanWriter(stackalloc byte[16]);
             writer.Write((byte)0x6F); // Packet ID
-            writer.Write((ushort)16); // Length
+            writer.Write((ushort)17); // Length
             writer.Write((byte)flag);
             writer.Write(cont.Serial);
             writer.Write(first);
             writer.Write(second);
+            writer.Write(false);
 
             ns.Send(writer.Span);
         }
