32220993b9965ae8696b6f70dc1523c9cba2d1ca
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-04-23 20:52:34 -0700

    Fixes decorate mag

--- Projects/UOContent/Commands/Object Creation/DecorateMag.cs
+++ Projects/UOContent/Commands/Object Creation/DecorateMag.cs
@@ -108,7 +108,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                labelNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                labelNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                                 break;
                             }
                         }
@@ -128,7 +128,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                labelNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                labelNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                                 break;
                             }
                         }
@@ -205,7 +205,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                range = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                range = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             }
                         }
                         else if (m_Params[i].StartsWithOrdinal("WarningString"))
@@ -223,7 +223,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                messageNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                messageNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             }
                         }
                         else if (m_Params[i].StartsWithOrdinal("HintString"))
@@ -241,7 +241,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                hintNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                hintNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             }
                         }
                         else if (m_Params[i].StartsWithOrdinal("ResetDelay"))
@@ -278,7 +278,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                range = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                range = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             }
                         }
                         else if (m_Params[i].StartsWithOrdinal("WarningString"))
@@ -296,7 +296,7 @@ namespace Server.Commands
 
                             if (indexOf >= 0)
                             {
-                                messageNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                                messageNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             }
                         }
                         else if (m_Params[i].StartsWithOrdinal("ResetDelay"))
@@ -556,7 +556,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            sp.Count = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            sp.Count = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                             for (var se = 0; se < sp.Entries.Count; se++)
                             {
                                 sp.Entries[se].SpawnedMaxCount = sp.Count;
@@ -569,7 +569,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            sp.Team = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            sp.Team = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("HomeRange"))
@@ -578,7 +578,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            sp.HomeRange = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            sp.HomeRange = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("Running"))
@@ -689,7 +689,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            st.MessageNumber = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            st.MessageNumber = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("PointDest"))
@@ -743,7 +743,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            st.SoundID = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            st.SoundID = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("Delay"))
@@ -781,7 +781,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            kt.Keyword = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            kt.Keyword = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("Range"))
@@ -790,7 +790,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            kt.Range = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            kt.Range = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("PointDest"))
@@ -844,7 +844,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            kt.SoundID = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            kt.SoundID = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("Delay"))
@@ -918,7 +918,7 @@ namespace Server.Commands
 
                         if (indexOf >= 0)
                         {
-                            tp.SoundID = Utility.ToInt32(m_Params[i][++indexOf..]);
+                            tp.SoundID = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         }
                     }
                     else if (m_Params[i].StartsWithOrdinal("Delay"))
@@ -985,7 +985,7 @@ namespace Server.Commands
 
                     if (indexOf >= 0)
                     {
-                        var hue = Utility.ToInt32(m_Params[i][++indexOf..]);
+                        var hue = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
 
                         if (item is DyeTub tub)
                         {
@@ -1017,7 +1017,7 @@ namespace Server.Commands
                         var wasStackable = item.Stackable;
 
                         item.Stackable = true;
-                        item.Amount = Utility.ToInt32(m_Params[i][++indexOf..]);
+                        item.Amount = Utility.ToInt32(m_Params[i].AsSpan()[++indexOf..]);
                         item.Stackable = wasStackable;
                     }
                 }
