d7a627e7ceafec63cd0b22a5bcb25a3dfa2c2dee
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-05-30 00:02:33 -0700

    Fixes external init issue

---
+++ Projects/SerializationSchemaGenerator/Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializationSchemaGenerator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
