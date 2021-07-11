8d646094447bb5f777622957caa0de0b83ac0ac7
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-07-11 15:17:05 -0700

    fix(core): Anchors delta min/max

--- Projects/Server/Serialization/IGenericReader.cs
+++ Projects/Server/Serialization/IGenericReader.cs
@@ -36,7 +36,17 @@ namespace Server
 
         DateTime ReadDateTime() => new(ReadLong(), DateTimeKind.Utc);
         TimeSpan ReadTimeSpan() => new(ReadLong());
-        DateTime ReadDeltaTime() => new(ReadLong() + DateTime.UtcNow.Ticks, DateTimeKind.Utc);
+
+        DateTime ReadDeltaTime()
+        {
+            var delta = ReadLong();
+            return delta switch
+            {
+                long.MinValue => DateTime.MinValue,
+                long.MaxValue => DateTime.MaxValue,
+                _             => new DateTime(delta + DateTime.UtcNow.Ticks, DateTimeKind.Utc)
+            };
+        }
         decimal ReadDecimal() => new(stackalloc int[4] { ReadInt(), ReadInt(), ReadInt(), ReadInt() });
         int ReadEncodedInt()
         {
