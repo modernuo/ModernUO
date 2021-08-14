2de47173901cc68f46c97ab442af748aa6534748
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-08-14 16:53:18 -0700

    fix(content): Adds addons to code gen (#624)
    * Codegens more addons.
    * Fixes up some source generation issues.

--- Projects/UOContent/Items/Addons/ArcanistStatueSouthAddon.cs
+++ Projects/UOContent/Items/Addons/ArcanistStatueSouthAddon.cs
@@ -1,6 +1,7 @@
 namespace Server.Items
 {
-    public class ArcanistStatueSouthAddon : BaseAddon
+    [Serializable(0)]
+    public partial class ArcanistStatueSouthAddon : BaseAddon
     {
         [Constructible]
         public ArcanistStatueSouthAddon()
@@ -8,53 +9,18 @@ namespace Server.Items
             AddComponent(new AddonComponent(0x2D0F), 0, 0, 0);
         }
 
-        public ArcanistStatueSouthAddon(Serial serial) : base(serial)
-        {
-        }
-
         public override BaseAddonDeed Deed => new ArcanistStatueSouthDeed();
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
     }
 
-    public class ArcanistStatueSouthDeed : BaseAddonDeed
+    [Serializable(0)]
+    public partial class ArcanistStatueSouthDeed : BaseAddonDeed
     {
         [Constructible]
         public ArcanistStatueSouthDeed()
         {
         }
 
-        public ArcanistStatueSouthDeed(Serial serial) : base(serial)
-        {
-        }
-
         public override BaseAddon Addon => new ArcanistStatueSouthAddon();
         public override int LabelNumber => 1072885; // arcanist statue (south)
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
     }
 }
