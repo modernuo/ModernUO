2d6e9871434fe749d4e95bc086aaaf78d4b5bbbd
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-08-14 18:24:31 -0700

    Codegens tubs

--- Projects/UOContent/Items/Skill Items/Tailor Items/Dyetubs/WhiteClothDyeTub.cs
+++ Projects/UOContent/Items/Skill Items/Tailor Items/Dyetubs/WhiteClothDyeTub.cs
@@ -1,31 +1,13 @@
 namespace Server.Items /* High seas, loot from merchant ship's hold, also a "uncommon" loot item */
 {
-    public class WhiteClothDyeTub : DyeTub
+    [Serializable(0, false)]
+    public partial class WhiteClothDyeTub : DyeTub
     {
         [Constructible]
         public WhiteClothDyeTub() => DyedHue = Hue = 0x9C2;
 
-        public WhiteClothDyeTub(Serial serial)
-            : base(serial)
-        {
-        }
-
         public override int LabelNumber => 1149984; // White Cloth Dye Tub
 
         public override bool Redyable => false;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-        }
     }
 }
