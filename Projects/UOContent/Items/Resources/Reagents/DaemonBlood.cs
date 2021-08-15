c1a75624de4dac24091bcbcbd3235ba9008019b3
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-08-14 18:41:42 -0700

    Codegens basic reagents

--- Projects/UOContent/Items/Resources/Reagents/DaemonBlood.cs
+++ Projects/UOContent/Items/Resources/Reagents/DaemonBlood.cs
@@ -1,31 +1,14 @@
 namespace Server.Items
 {
-    public class DaemonBlood : BaseReagent, ICommodity
+    [Serializable(0, false)]
+    public partial class DaemonBlood : BaseReagent, ICommodity
     {
         [Constructible]
         public DaemonBlood(int amount = 1) : base(0xF7D, amount)
         {
         }
 
-        public DaemonBlood(Serial serial) : base(serial)
-        {
-        }
-
         int ICommodity.DescriptionNumber => LabelNumber;
         bool ICommodity.IsDeedable => true;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-        }
     }
 }
--- Projects/UOContent/Items/Resources/Reagents/DeadWood.cs
+++ Projects/UOContent/Items/Resources/Reagents/DeadWood.cs
@@ -1,31 +1,14 @@
 namespace Server.Items
 {
-    public class DeadWood : BaseReagent, ICommodity
+    [Serializable(0, false)]
+    public partial class DeadWood : BaseReagent, ICommodity
     {
         [Constructible]
         public DeadWood(int amount = 1) : base(0xF90, amount)
         {
         }
 
-        public DeadWood(Serial serial) : base(serial)
-        {
-        }
-
         int ICommodity.DescriptionNumber => LabelNumber;
         bool ICommodity.IsDeedable => true;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-        }
     }
 }
