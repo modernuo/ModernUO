bdac50881eb21ebec49770689dbac9ded146f618
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-08-21 00:12:58 -0700

    fix(network): Don't crash the user when they click the UO Store button

---
+++ Projects/UOContent/Engines/UltimaStore/UltimaStorePackets.cs
@@ -0,0 +1,17 @@
+using Server.Network;
+
+namespace Server.Engines.UltimaStore
+{
+    public static class UltimaStorePackets
+    {
+        public static void Configure()
+        {
+            IncomingPackets.Register(0xFA, 1, true, UltimaStoreOpenRequest);
+        }
+
+        public static void UltimaStoreOpenRequest(NetState state, CircularBufferReader reader, ref int packetLength)
+        {
+            state.Mobile.SendMessage("Ultima Store is not currently available.");
+        }
+    }
+}
