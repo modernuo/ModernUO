8b0eb1aa79c209ef310a53824beb1addfc659da7
Michael Dijkstra <micdijkstra@gmail.com>
2021-08-22 18:30:27 +1000

    fix(codegen): Codegens wands (#716)

--- Projects/UOContent/Items/Wands/HarmWand.cs
+++ Projects/UOContent/Items/Wands/HarmWand.cs
@@ -2,29 +2,12 @@ using Server.Spells.Second;
 
 namespace Server.Items
 {
-    public class HarmWand : BaseWand
+    [Serializable(0, false)]
+    public partial class HarmWand : BaseWand
     {
         [Constructible]
         public HarmWand() : base(WandEffect.Harming, 5, Core.ML ? 109 : 30)
         {
-        }
-
-        public HarmWand(Serial serial) : base(serial)
-        {
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
         }
 
         public override void OnWandUse(Mobile from)
