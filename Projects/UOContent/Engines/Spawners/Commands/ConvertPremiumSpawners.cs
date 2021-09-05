7c904f0ee6d57e3d337515bec8ce76c2c09a4aa9
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-09-04 18:11:10 -0700

    fix: Fixes spawners

--- Projects/UOContent/Engines/Spawners/Commands/ConvertPremiumSpawners.cs
+++ Projects/UOContent/Engines/Spawners/Commands/ConvertPremiumSpawners.cs
@@ -16,16 +16,20 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using Server.Json;
+using Server.Logging;
 using Server.Network;
 
 namespace Server.Engines.Spawners
 {
     public static class ConvertPremiumSpawners
     {
+        private static readonly ILogger logger = LogFactory.GetLogger(typeof(ConvertPremiumSpawners));
+
         public static void Initialize()
         {
             CommandSystem.Register("ConvertPremiumSpawners", AccessLevel.Developer, ConvertPremiumSpawners_OnCommand);
@@ -48,10 +52,10 @@ namespace Server.Engines.Spawners
                 .Execute(new DirectoryInfoWrapper(inputDi))
                 .Files;
 
-            List<FileInfo> files = new List<FileInfo>();
+            List<(FileInfo, string)> files = new List<(FileInfo, string)>();
             foreach (var match in patternMatches)
             {
-                files.Add(new FileInfo(match.Path));
+                files.Add((new FileInfo(match.Path), match.Stem));
             }
 
             if (files.Count == 0)
@@ -60,38 +64,38 @@ namespace Server.Engines.Spawners
                 return;
             }
 
-            var inputDir = Path.Combine(Core.BaseDirectory, args.Arguments[0]);
             var outputDir = Path.Combine(Core.BaseDirectory, args.Arguments[1]);
             var options = JsonConfig.GetOptions(new TextDefinitionConverterFactory());
 
             for (var i = 0; i < files.Count; i++)
             {
-                var file = files[i];
-                from.SendMessage("ConvertPremiumSpawners: Converting spawners for {0}...", file.Name);
+                var (file, stem) = files[i];
+                from.SendMessage("ConvertPremiumSpawners: Converting spawners for {0}...", stem);
 
                 NetState.FlushAll();
 
                 try
                 {
-                    var relativePath = Path.GetRelativePath(inputDir, file.DirectoryName!);
-                    var fullOutputDir = Path.Combine(outputDir, relativePath);
+                    var stemDir = stem[..^file.Name.Length];
+                    var fullOutputDir = Path.Combine(outputDir, stemDir);
                     AssemblyHandler.EnsureDirectory(fullOutputDir);
-                    ParsePremiumSpawnerFile(file, fullOutputDir, options);
+
+                    var outputFileName = $"{file.Name[..^file.Extension.Length]}.json";
+                    Console.WriteLine("Output file: {0}", outputFileName);
+
+                    // ParsePremiumSpawnerFile(file.FullName, Path.Combine(fullOutputDir, outputFileName), options);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
-                    from.SendMessage(
-                        "ConvertPremiumSpawners: Exception parsing {0}, file may not be in the correct format.",
-                        file.FullName
-                    );
+                    logger.Error(e.ToString());
+                    from.SendMessage($"ConvertPremiumSpawners: Exception parsing {stem}, file may not be in the correct format.");
                 }
             }
         }
 
-        private static void ParsePremiumSpawnerFile(FileInfo file, string outputDirectory, JsonSerializerOptions options)
+        private static void ParsePremiumSpawnerFile(string inputFile, string outputFile, JsonSerializerOptions options)
         {
-            var lines = File.ReadAllLines(file.FullName);
+            var lines = File.ReadAllLines(inputFile);
 
             TimeSpan minTime = TimeSpan.MinValue;
             TimeSpan maxTime = TimeSpan.MinValue;
@@ -153,8 +157,6 @@ namespace Server.Engines.Spawners
                 }
             }
 
-            var outputFile = Path.Combine(outputDirectory, $"{file.Name[..^file.Extension.Length]}.json");
-            Console.WriteLine("Writing to: {0}", outputFile);
             JsonConfig.Serialize(outputFile, json, options);
         }
 
@@ -188,7 +190,7 @@ namespace Server.Engines.Spawners
 
                 for (var i = 0; i < 6; i++)
                 {
-                    spawner.Entries.AddRange(CreateSpawnerEntries(parts[i + 1], int.Parse(parts[i + 15])));
+                    spawner.Entries.AddRange(CreateSpawnerEntries(parts[i + 1], int.Parse(parts[i + 16])));
                 }
 
                 if (spawner.Entries.Count == 0)
--- Projects/UOContent/Items/Misc/Teleporter.cs
+++ Projects/UOContent/Items/Misc/Teleporter.cs
@@ -671,7 +671,7 @@ namespace Server.Items
 
     public class WaitTeleporter : KeywordTeleporter
     {
-        private static Dictionary<Mobile, TeleportingInfo> m_Table = new Dictionary<Mobile, TeleportingInfo>();
+        private static Dictionary<Mobile, TeleportingInfo> m_Table = new();
 
         [Constructible]
         public WaitTeleporter()
