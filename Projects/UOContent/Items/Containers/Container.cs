cf8e4f9f29b48bd9c646107a6efe6acbe3f11660
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-09-05 20:39:41 -0700

    fix: Codegens chairs

--- Projects/UOContent/Items/Containers/Container.cs
+++ Projects/UOContent/Items/Containers/Container.cs
@@ -712,7 +712,7 @@ namespace Server.Items
         }
     }
 
-    [DynamicFliping, Flippable(0x9A8, 0xE80)]
+    [DynamicFlipping, Flippable(0x9A8, 0xE80)]
     public class MetalBox : LockableContainer
     {
         [Constructible]
@@ -744,7 +744,7 @@ namespace Server.Items
         }
     }
 
-    [DynamicFliping, Flippable(0x9AB, 0xE7C)]
+    [DynamicFlipping, Flippable(0x9AB, 0xE7C)]
     public class MetalChest : LockableContainer
     {
         [Constructible]
@@ -776,7 +776,7 @@ namespace Server.Items
         }
     }
 
-    [DynamicFliping, Flippable(0xE41, 0xE40)]
+    [DynamicFlipping, Flippable(0xE41, 0xE40)]
     public class MetalGoldenChest : LockableContainer
     {
         [Constructible]
