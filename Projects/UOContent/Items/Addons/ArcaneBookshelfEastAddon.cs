363208bbad76b2fcc5ed5a40419df84f02ee789b
Arthrutus <quilnn@hotmail.com>
2021-09-13 21:20:25 -0500

    Updates

--- Projects/UOContent/Items/Addons/ArcaneBookshelfEastAddon.cs
+++ Projects/UOContent/Items/Addons/ArcaneBookshelfEastAddon.cs
@@ -1,22 +1,27 @@
 namespace Server.Items
 {
     [Serializable(0)]
+    public partial class ArcaneBookshelfEastAddon : BaseAddon
     public partial class ArcaneBookshelfEastAddon : BaseAddonContainer
     {
         [Constructible]
-        public ArcaneBookshelfEastAddon() : base(0x2DF0)
+        public ArcaneBookshelfEastAddon()
+        public ArcaneBookshelfEastAddon() : base(0x3084)
         {
-            AddComponent(new LocalizedContainerComponent(0x21A4, 1031760), 0, -1, 0);
+            AddComponent(new AddonComponent(0x3084), 0, 0, 0);
+            AddComponent(new AddonComponent(0x3085), -1, 0, 0);
+            AddComponent(new AddonContainerComponent(0x3085), -1, 0, 0);
         }
 
+        public override BaseAddonDeed Deed => new ArcaneBookshelfEastDeed();
         public override BaseAddonContainerDeed Deed => new ArcaneBookshelfEastDeed();
         public override bool RetainDeedHue => true;
         public override int DefaultGumpID => 0x107;
         public override int DefaultDropSound => 0x42;
-        public override int LabelNumber => 1032420; // arcane bookshelf
     }
 
     [Serializable(0)]
+    public partial class ArcaneBookshelfEastDeed : BaseAddonDeed
     public partial class ArcaneBookshelfEastDeed : BaseAddonContainerDeed
     {
         [Constructible]
@@ -24,6 +29,7 @@ namespace Server.Items
         {
         }
 
+        public override BaseAddon Addon => new ArcaneBookshelfEastAddon();
         public override BaseAddonContainer Addon => new ArcaneBookshelfEastAddon();
         public override int LabelNumber => 1073371; // arcane bookshelf (east)
     }
