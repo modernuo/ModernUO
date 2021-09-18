006db09c17461196f303017054ac72164c98d892
IamDanielDinner <danekvecera@gmail.com>
2021-09-18 20:01:51 +0200

    Changes out to ref

--- Projects/UOContent/Items/Minor Artifacts/ML/QuiverOfRage.cs
+++ Projects/UOContent/Items/Minor Artifacts/ML/QuiverOfRage.cs
@@ -18,8 +18,8 @@ namespace Server.Items
         public override int LabelNumber => 1075038; // Quiver of Rage
 
         public override void AlterBowDamage(
-            out int phys, out int fire, out int cold, out int pois, out int nrgy,
-            out int chaos, out int direct
+            ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
+            ref int chaos, ref int direct
         )
         {
             chaos = direct = 0;
