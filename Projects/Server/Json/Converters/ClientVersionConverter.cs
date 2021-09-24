2f5b26ae6856fda41c4b3fd80420a610b6f7e6a1
Michael Whelehan <michael.marula+1@gmail.com>
2021-09-24 05:28:43 +0200

    feat(expansions): Adds expansion.json and loads expansions dynamically (#798)
    Moves hardcoded expansion settings to the file Data\expansion.json.

---
+++ Projects/Server/Json/Converters/ClientVersionConverter.cs
@@ -0,0 +1,37 @@
+/*************************************************************************
+ * ModernUO                                                              *
+ * Copyright 2021 - ModernUO Development Team                            *
+ * Email: hi@modernuo.com                                                *
+ * File: ClientVersionConverter.cs                                       *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Server.Json
+{
+    public class ClientVersionConverter : JsonConverter<ClientVersion>
+    {
+        public override ClientVersion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                return new ClientVersion(reader.GetString());
+            }
+
+            throw new JsonException($"Value must be a string");
+        }
+
+        public override void Write(Utf8JsonWriter writer, ClientVersion value, JsonSerializerOptions options) =>
+            writer.WriteStringValue(value.ToString());
+    }
+}
