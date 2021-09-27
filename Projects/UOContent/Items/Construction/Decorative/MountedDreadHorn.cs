2253a2332c557283230babd6270c6975a4174c0b
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-09-26 18:54:33 -0700

    fix: Fixes ML containers and addons (#804)
    * Fixes some ML addons so they are containers
    * Adds some missing ML artifacts
    * Fixes carpentry gump crafting entries

---
+++ Projects/UOContent/Items/Construction/Decorative/MountedDreadHorn.cs
@@ -0,0 +1,12 @@
+namespace Server.Items
+{
+    [Flippable(0x3158, 0x3159)]
+    [Serializable(0)]
+    public partial class MountedDreadHorn : Item
+    {
+        [Constructible]
+        public MountedDreadHorn() : base(0x3158) => Weight = 1.0;
+
+        public override int LabelNumber => 1074464; // mounted Dread Horn
+    }
+}
---
+++ Projects/UOContent/Items/Decoration Artifacts/AcademicBooksArtifacts.cs
@@ -0,0 +1,12 @@
+namespace Server.Items
+{
+    [Serializable(0)]
+    public partial class AcademicBooksArtifact : BaseDecorationArtifact
+    {
+        public override int ArtifactRarity => 8;
+        public override int LabelNumber => 1071202;  // academic books
+
+        [Constructible]
+        public AcademicBooksArtifact() : base(0x1E25) => Hue = 2413;
+    }
+}
