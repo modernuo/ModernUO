2f77a32043fc75a541cb4f46556feb6cbfe96529
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-09-26 19:17:42 -0700

    Adds Benches

--- Projects/UOContent/Items/Construction/Chairs/Benchs.cs
+++ Projects/UOContent/Items/Construction/Chairs/Benchs.cs
@@ -3,7 +3,7 @@ namespace Server.Items
     [Furniture]
     [Serializable(0, false)]
     [Flippable(0xB2D, 0xB2C)]
-    public class WoodenBench : Item
+    public partial class WoodenBench : Item
     {
         [Constructible]
         public WoodenBench() : base(0xB2D) => Weight = 6;
