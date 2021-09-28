7d6c4edbd410b5570188829e8dc080d99712f2c8
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-09-27 20:54:42 -0700

    Removes console log

--- Projects/Server/Items/Item.cs
+++ Projects/Server/Items/Item.cs
@@ -2660,11 +2660,6 @@ namespace Server
                             m_ItemID = reader.ReadEncodedInt();
                         }
 
-                        if (GetType().Name == "ArmorOfFortune")
-                        {
-                            Console.WriteLine("AoF");
-                        }
-
                         if (GetSaveFlag(flags, SaveFlag.Hue))
                         {
                             m_Hue = reader.ReadEncodedInt();
