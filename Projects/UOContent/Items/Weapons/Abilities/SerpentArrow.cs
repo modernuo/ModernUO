872cae4aefb64405cf98f616f70d09ace36dfef0
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-10-03 23:01:06 -0700

    Update SerpentArrow.cs

--- Projects/UOContent/Items/Weapons/Abilities/SerpentArrow.cs
+++ Projects/UOContent/Items/Weapons/Abilities/SerpentArrow.cs
@@ -4,7 +4,7 @@ namespace Server.Items
     {
         public override int BaseMana => 25;
 
-        //Serpent arrow is used only as secondary ability on "Elven composite longbow" so needed skill is always 60, but for sure we will use GetRequiredSecondarySkill
+        // Serpent arrow is used only as secondary ability on "Elven composite longbow" so needed skill is always 60, but for sure we will use GetRequiredSecondarySkill
         public override bool RequiresSecondarySkill(Mobile from) => true;
         public override double GetRequiredSecondarySkill(Mobile from) => 60;
         public override SkillName GetSecondarySkillName(Mobile from) => SkillName.Poisoning;
@@ -18,7 +18,7 @@ namespace Server.Items
 
             ClearCurrentAbility(attacker);
 
-            defender.SendLocalizedMessage(1112369); //You have been poisoned by a lethal arrow!
+            defender.SendLocalizedMessage(1112369); // You have been poisoned by a lethal arrow!
 
             int level;
 
