e84fa02b98e3ac9a054259825c0e41e4ec1dbcc8
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-10-05 20:05:05 -0700

    fix: Fixes epoll calls on linux

--- Projects/Server/Network/PollGroup/EPollGroup.cs
+++ Projects/Server/Network/PollGroup/EPollGroup.cs
@@ -93,16 +93,16 @@ namespace Server.Network
 
         private static class Linux
         {
-            [DllImport("libc", SetLastError = true)]
+            [DllImport("libc.so.6", SetLastError = true)]
             public static extern int epoll_create1(epoll_flags flags);
 
-            [DllImport("libc", SetLastError = true)]
+            [DllImport("libc.so.6", SetLastError = true)]
             public static extern int epoll_close(int epfd);
 
-            [DllImport("libc", SetLastError = true)]
+            [DllImport("libc.so.6", SetLastError = true)]
             public static extern int epoll_ctl(int epfd, epoll_op op, int fd, ref epoll_event ee);
 
-            [DllImport("libc", SetLastError = true)]
+            [DllImport("libc.so.6", SetLastError = true)]
             public static extern int epoll_wait(int epfd, [In, Out] epoll_event[] ee, int maxevents, int timeout);
         }
 
