58190674f9970944d6c15030a5d047de9519d114
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-12-27 01:27:15 -0800

    feat: Adds StaffAccess and ResetStaffDress commands (#895)

--- Projects/UOContent/Commands/ShardTime.cs
+++ Projects/UOContent/Commands/ShardTime.cs
@@ -9,7 +9,8 @@ namespace Server.Commands
             CommandSystem.Register("Time", AccessLevel.Player, Time_OnCommand);
         }
 
-        [Usage("Time"), Description("Returns the server's local time.")]
+        [Usage("Time")]
+        [Description("Returns the server's local time.")]
         private static void Time_OnCommand(CommandEventArgs e)
         {
             e.Mobile.SendMessage(Core.Now.ToString(CultureInfo.InvariantCulture));
