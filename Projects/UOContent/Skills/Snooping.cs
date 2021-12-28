02d0a6f6f9a119617da3dd30181b998ceb48cbae
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-12-27 19:53:29 -0800

    fix: Fixes snooping staff

--- Projects/UOContent/Skills/Snooping.cs
+++ Projects/UOContent/Skills/Snooping.cs
@@ -3,68 +3,63 @@ using Server.Misc;
 using Server.Mobiles;
 using Server.Regions;
 
-namespace Server.SkillHandlers
+namespace Server.SkillHandlers;
+
+public static class Snooping
 {
-    public static class Snooping
+    public static void Configure()
     {
-        public static void Configure()
+        Container.SnoopHandler = Container_Snoop;
+    }
+
+    public static bool CheckSnoopAllowed(Mobile from, Mobile to)
+    {
+        var map = from.Map;
+
+        if (to.Player)
         {
-            Container.SnoopHandler = Container_Snoop;
+            return from.CanBeHarmful(to, false, true); // normal restrictions
         }
 
-        public static bool CheckSnoopAllowed(Mobile from, Mobile to)
+        if ((map?.Rules & MapRules.HarmfulRestrictions) == 0)
         {
-            var map = from.Map;
-
-            if (to.Player)
-            {
-                return from.CanBeHarmful(to, false, true); // normal restrictions
-            }
-
-            if ((map?.Rules & MapRules.HarmfulRestrictions) == 0)
-            {
-                return true; // felucca you can snoop anybody
-            }
-
-            var reg = to.Region.GetRegion<GuardedRegion>();
-
-            if (reg?.IsDisabled() != true)
-            {
-                return true; // not in town? we can snoop any npc
-            }
-
-            return !to.Body.IsHuman || to is BaseCreature cret && (cret.AlwaysAttackable || cret.AlwaysMurderer);
+            return true; // felucca you can snoop anybody
         }
 
-        public static void Container_Snoop(Container cont, Mobile from)
+        var reg = to.Region.GetRegion<GuardedRegion>();
+
+        if (reg?.IsDisabled() != true)
         {
-            if (from.AccessLevel <= AccessLevel.Player && !from.InRange(cont.GetWorldLocation(), 1))
-            {
-                from.SendLocalizedMessage(500446); // That is too far away.
-                return;
-            }
+            return true; // not in town? we can snoop any npc
+        }
 
-            var root = cont.RootParent as Mobile;
+        return !to.Body.IsHuman || to is BaseCreature cret && (cret.AlwaysAttackable || cret.AlwaysMurderer);
+    }
 
-            if (root?.Alive == false)
-            {
-                return;
-            }
+    public static void Container_Snoop(Container cont, Mobile from)
+    {
+        if (from.AccessLevel <= AccessLevel.Player && !from.InRange(cont.GetWorldLocation(), 1))
+        {
+            from.SendLocalizedMessage(500446); // That is too far away.
+            return;
+        }
 
-            if (root?.AccessLevel > AccessLevel.Player && from.AccessLevel == AccessLevel.Player)
-            {
-                from.SendLocalizedMessage(500209); // You can not peek into the container.
-                return;
-            }
+        var root = cont.RootParent as Mobile;
 
-            if (root?.AccessLevel == AccessLevel.Player && !CheckSnoopAllowed(from, root))
-            {
-                from.SendLocalizedMessage(1001018); // You cannot perform negative acts on your target.
-                return;
-            }
+        if (root?.Alive == false)
+        {
+            return;
+        }
 
-            if (root?.AccessLevel == AccessLevel.Player &&
-                from.Skills.Snooping.Value < Utility.Random(100))
+        if (root?.AccessLevel > AccessLevel.Player || !CheckSnoopAllowed(from, root))
+        {
+            from.SendLocalizedMessage(1001018); // You cannot perform negative acts on your target.
+            return;
+        }
+
+        if (from.AccessLevel == AccessLevel.Player)
+        {
+            if (root != null && from.Skills.Snooping.Value < 100.0 && from.Skills.Snooping.Value < Utility.Random(100))
             {
                 var map = from.Map;
 
@@ -86,28 +81,25 @@ namespace Server.SkillHandlers
                 }
             }
 
-            if (from.AccessLevel == AccessLevel.Player)
+            Titles.AwardKarma(from, -4, true);
+        }
+
+        if (from.AccessLevel > AccessLevel.Counselor || from.CheckTargetSkill(SkillName.Snooping, cont, 0.0, 100.0))
+        {
+            if (cont is TrappableContainer container && container.ExecuteTrap(from))
             {
-                Titles.AwardKarma(from, -4, true);
+                return;
             }
 
-            if (from.AccessLevel > AccessLevel.Player || from.CheckTargetSkill(SkillName.Snooping, cont, 0.0, 100.0))
-            {
-                if (cont is TrappableContainer container && container.ExecuteTrap(from))
-                {
-                    return;
-                }
+            cont.DisplayTo(from);
+        }
+        else
+        {
+            from.SendLocalizedMessage(500210); // You failed to peek into the container.
 
-                cont.DisplayTo(from);
-            }
-            else
+            if (from.Skills.Hiding.Fixed / 2 < Utility.Random(1000))
             {
-                from.SendLocalizedMessage(500210); // You failed to peek into the container.
-
-                if (from.Skills.Hiding.Value / 2 < Utility.Random(100))
-                {
-                    from.RevealingAction();
-                }
+                from.RevealingAction();
             }
         }
     }
