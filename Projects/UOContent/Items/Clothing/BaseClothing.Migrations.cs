4baafb21c3ce06b6296d890493da7a400e9f647f
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-02-12 18:39:49 -0800

    Cleanup migrations

--- Projects/UOContent/Items/Clothing/BaseClothing.Migrations.cs
+++ Projects/UOContent/Items/Clothing/BaseClothing.Migrations.cs
@@ -15,4 +15,74 @@ public partial class BaseClothing
         _quality = content.Quality ?? ClothingQuality.Regular;
         _strReq = content.StrRequirement ?? -1;
     }
+
+    // Version 5 (pre-codegen)
+    private void Deserialize(IGenericReader reader, int version)
+    {
+        var flags = (OldSaveFlag)reader.ReadEncodedInt();
+
+        if (GetSaveFlag(flags, OldSaveFlag.Resource))
+        {
+            _rawResource = (CraftResource)reader.ReadEncodedInt();
+        }
+        else
+        {
+            _rawResource = DefaultResource;
+        }
+
+        Attributes = new AosAttributes(this);
+
+        if (GetSaveFlag(flags, OldSaveFlag.Attributes))
+        {
+            Attributes.Deserialize(reader);
+        }
+
+        ClothingAttributes = new AosArmorAttributes(this);
+
+        if (GetSaveFlag(flags, OldSaveFlag.ClothingAttributes))
+        {
+            ClothingAttributes.Deserialize(reader);
+        }
+
+        SkillBonuses = new AosSkillBonuses(this);
+
+        if (GetSaveFlag(flags, OldSaveFlag.SkillBonuses))
+        {
+            SkillBonuses.Deserialize(reader);
+        }
+
+        Resistances = new AosElementAttributes(this);
+
+        if (GetSaveFlag(flags, OldSaveFlag.Resistances))
+        {
+            Resistances.Deserialize(reader);
+        }
+
+        if (GetSaveFlag(flags, OldSaveFlag.MaxHitPoints))
+        {
+            _maxHitPoints = reader.ReadEncodedInt();
+        }
+
+        if (GetSaveFlag(flags, OldSaveFlag.HitPoints))
+        {
+            _hitPoints = reader.ReadEncodedInt();
+        }
+
+        if (GetSaveFlag(flags, OldSaveFlag.Crafter))
+        {
+            _crafter = reader.ReadEntity<Mobile>()?.RawName;
+        }
+
+        if (GetSaveFlag(flags, OldSaveFlag.Quality))
+        {
+            _quality = (ClothingQuality)reader.ReadEncodedInt();
+        }
+
+        if (GetSaveFlag(flags, OldSaveFlag.StrReq))
+        {
+            _strReq = reader.ReadEncodedInt();
+        }
+
+        PlayerConstructed = GetSaveFlag(flags, OldSaveFlag.PlayerConstructed);
+    }
 }
