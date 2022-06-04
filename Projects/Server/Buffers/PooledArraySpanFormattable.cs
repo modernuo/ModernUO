5f00330a66dbce60856ee026209ab47c43fd5875
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-04 11:53:32 -0700

    fix: Adds optimized formatter for clilocs (#1045)
    Adds an optimized formatter for localization. Example:
    ```cs
    string localizationText = Localization.Format(1050039, "enu", $"{m_Amount}\t{LabelNumber:#}");
    ```
    
    Fixes #1044

--- Projects/Server/Buffers/PooledArraySpanFormattable.cs
+++ Projects/Server/Buffers/PooledArraySpanFormattable.cs
@@ -22,11 +22,13 @@ public struct PooledArraySpanFormattable : ISpanFormattable, IDisposable
 {
     private char[] _arrayToReturnToPool;
     private int _pos;
+    private string _value;
 
     public PooledArraySpanFormattable(char[] arrayToReturnToPool, int length)
     {
         _arrayToReturnToPool = arrayToReturnToPool;
         _pos = length;
+        _value = null;
     }
 
     public ReadOnlySpan<char> Chars => _arrayToReturnToPool.AsSpan(.._pos);
@@ -35,10 +37,12 @@ public struct PooledArraySpanFormattable : ISpanFormattable, IDisposable
 
     public string ToString(string? format = null, IFormatProvider formatProvider = null)
     {
-        var result = new string(_arrayToReturnToPool.AsSpan(0, _pos));
-        Dispose();
+        _value ??= new string(_arrayToReturnToPool.AsSpan(0, _pos));
 
-        return result;
+        STArrayPool<char>.Shared.Return(_arrayToReturnToPool);
+        _arrayToReturnToPool = null;
+
+        return _value;
     }
 
     public bool TryFormat(
@@ -53,18 +57,14 @@ public struct PooledArraySpanFormattable : ISpanFormattable, IDisposable
         }
 
         _arrayToReturnToPool.AsSpan(0, _pos).CopyTo(destination);
-        Dispose();
-
         charsWritten = _pos;
         return true;
     }
 
     public void Dispose()
     {
-        if (_arrayToReturnToPool != null)
-        {
-            STArrayPool<char>.Shared.Return(_arrayToReturnToPool);
-            _arrayToReturnToPool = null;
-        }
+        STArrayPool<char>.Shared.Return(_arrayToReturnToPool);
+        _arrayToReturnToPool = null;
+        this = default; // Defensive clear
     }
 }
