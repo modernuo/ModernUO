ceb84bd4d93aa276b6b4f846e3ef95fe3cf01724
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-13 17:30:25 -0700

    fix: Codegens games

--- Projects/UOContent/Items/Games/ChessPieces.cs
+++ Projects/UOContent/Items/Games/ChessPieces.cs
@@ -1,302 +1,123 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhiteKing : BasePiece
 {
-    public class PieceWhiteKing : BasePiece
+    public PieceWhiteKing(BaseBoard board) : base(0x3587, board)
     {
-        public PieceWhiteKing(BaseBoard board) : base(0x3587, board)
-        {
-        }
-
-        public PieceWhiteKing(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white king";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackKing : BasePiece
+    public override string DefaultName => "white king";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackKing : BasePiece
+{
+    public PieceBlackKing(BaseBoard board) : base(0x358E, board)
     {
-        public PieceBlackKing(BaseBoard board) : base(0x358E, board)
-        {
-        }
-
-        public PieceBlackKing(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black king";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceWhiteQueen : BasePiece
+    public override string DefaultName => "black king";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhiteQueen : BasePiece
+{
+    public PieceWhiteQueen(BaseBoard board) : base(0x358A, board)
     {
-        public PieceWhiteQueen(BaseBoard board) : base(0x358A, board)
-        {
-        }
-
-        public PieceWhiteQueen(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white queen";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackQueen : BasePiece
+    public override string DefaultName => "white queen";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackQueen : BasePiece
+{
+    public PieceBlackQueen(BaseBoard board) : base(0x3591, board)
     {
-        public PieceBlackQueen(BaseBoard board) : base(0x3591, board)
-        {
-        }
-
-        public PieceBlackQueen(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black queen";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceWhiteRook : BasePiece
+    public override string DefaultName => "black queen";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhiteRook : BasePiece
+{
+    public PieceWhiteRook(BaseBoard board) : base(0x3586, board)
     {
-        public PieceWhiteRook(BaseBoard board) : base(0x3586, board)
-        {
-        }
-
-        public PieceWhiteRook(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white rook";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackRook : BasePiece
+    public override string DefaultName => "white rook";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackRook : BasePiece
+{
+    public PieceBlackRook(BaseBoard board) : base(0x358D, board)
     {
-        public PieceBlackRook(BaseBoard board) : base(0x358D, board)
-        {
-        }
-
-        public PieceBlackRook(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black rook";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceWhiteBishop : BasePiece
+    public override string DefaultName => "black rook";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhiteBishop : BasePiece
+{
+    public PieceWhiteBishop(BaseBoard board) : base(0x3585, board)
     {
-        public PieceWhiteBishop(BaseBoard board) : base(0x3585, board)
-        {
-        }
-
-        public PieceWhiteBishop(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white bishop";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackBishop : BasePiece
+    public override string DefaultName => "white bishop";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackBishop : BasePiece
+{
+    public PieceBlackBishop(BaseBoard board) : base(0x358C, board)
     {
-        public PieceBlackBishop(BaseBoard board) : base(0x358C, board)
-        {
-        }
-
-        public PieceBlackBishop(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black bishop";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceWhiteKnight : BasePiece
+    public override string DefaultName => "black bishop";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhiteKnight : BasePiece
+{
+    public PieceWhiteKnight(BaseBoard board) : base(0x3588, board)
     {
-        public PieceWhiteKnight(BaseBoard board) : base(0x3588, board)
-        {
-        }
-
-        public PieceWhiteKnight(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white knight";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackKnight : BasePiece
+    public override string DefaultName => "white knight";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackKnight : BasePiece
+{
+    public PieceBlackKnight(BaseBoard board) : base(0x358F, board)
     {
-        public PieceBlackKnight(BaseBoard board) : base(0x358F, board)
-        {
-        }
-
-        public PieceBlackKnight(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black knight";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceWhitePawn : BasePiece
+    public override string DefaultName => "black knight";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceWhitePawn : BasePiece
+{
+    public PieceWhitePawn(BaseBoard board) : base(0x3589, board)
     {
-        public PieceWhitePawn(BaseBoard board) : base(0x3589, board)
-        {
-        }
-
-        public PieceWhitePawn(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "white pawn";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
 
-    public class PieceBlackPawn : BasePiece
+    public override string DefaultName => "white pawn";
+}
+
+[SerializationGenerator(0, false)]
+public partial class PieceBlackPawn : BasePiece
+{
+    public PieceBlackPawn(BaseBoard board) : base(0x3590, board)
     {
-        public PieceBlackPawn(BaseBoard board) : base(0x3590, board)
-        {
-        }
-
-        public PieceBlackPawn(Serial serial) : base(serial)
-        {
-        }
-
-        public override string DefaultName => "black pawn";
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
     }
+
+    public override string DefaultName => "black pawn";
 }
