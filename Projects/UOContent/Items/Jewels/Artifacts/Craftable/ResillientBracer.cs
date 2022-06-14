c1cc1308a5a35f76140b929a081677d7f9ae8e60
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-14 08:29:00 -0700

    fix: Codegens guildstone and jewelery (#1061)

--- Projects/UOContent/Items/Jewels/Artifacts/Craftable/ResillientBracer.cs
+++ Projects/UOContent/Items/Jewels/Artifacts/Craftable/ResillientBracer.cs
@@ -1,39 +1,23 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class ResilientBracer : GoldBracelet
 {
-    public class ResilientBracer : GoldBracelet
+    [Constructible]
+    public ResilientBracer()
     {
-        [Constructible]
-        public ResilientBracer()
-        {
-            Hue = 0x488;
+        Hue = 0x488;
 
-            SkillBonuses.SetValues(0, SkillName.MagicResist, 15.0);
+        SkillBonuses.SetValues(0, SkillName.MagicResist, 15.0);
 
-            Attributes.BonusHits = 5;
-            Attributes.RegenHits = 2;
-            Attributes.DefendChance = 10;
-        }
-
-        public ResilientBracer(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1072933; // Resillient Bracer
-
-        public override int PhysicalResistance => 20;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        Attributes.BonusHits = 5;
+        Attributes.RegenHits = 2;
+        Attributes.DefendChance = 10;
     }
+
+    public override int LabelNumber => 1072933; // Resillient Bracer
+
+    public override int PhysicalResistance => 20;
 }
