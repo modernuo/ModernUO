a07c42fc853d2a65fc24bd82eda11b3ccac06ec7
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-13 17:38:46 -0700

    fix: Codegens gems (#1059)

--- Projects/UOContent/Items/Gems/Diamond.cs
+++ Projects/UOContent/Items/Gems/Diamond.cs
@@ -1,32 +1,16 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class Diamond : Item
 {
-    public class Diamond : Item
+    [Constructible]
+    public Diamond(int amount = 1) : base(0xF26)
     {
-        [Constructible]
-        public Diamond(int amount = 1) : base(0xF26)
-        {
-            Stackable = true;
-            Amount = amount;
-        }
-
-        public Diamond(Serial serial) : base(serial)
-        {
-        }
-
-        public override double DefaultWeight => 0.1;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-        }
+        Stackable = true;
+        Amount = amount;
     }
+
+    public override double DefaultWeight => 0.1;
 }
