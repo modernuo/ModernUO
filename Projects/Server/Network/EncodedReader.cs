f4158896eea4e02a2c761c03801fc5f27ef91a53
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-15 17:19:59 -0700

    Cleanup

--- Projects/Server/Network/EncodedReader.cs
+++ Projects/Server/Network/EncodedReader.cs
@@ -17,24 +17,21 @@ namespace Server.Network;
 
 public ref struct EncodedReader
 {
-    private CircularBufferReader m_Reader;
+    private CircularBufferReader _reader;
 
-    public EncodedReader(CircularBufferReader reader) => m_Reader = reader;
+    public EncodedReader(CircularBufferReader reader) => _reader = reader;
 
-    public void Trace(NetState state)
-    {
-        m_Reader.Trace(state);
-    }
+    public void Trace(NetState state) => _reader.Trace(state);
 
-    public int ReadInt32() => m_Reader.ReadByte() != 0 ? 0 : m_Reader.ReadInt32();
+    public int ReadInt32() => _reader.ReadByte() != 0 ? 0 : _reader.ReadInt32();
 
-    public Point3D ReadPoint3D() => m_Reader.ReadByte() != 3
+    public Point3D ReadPoint3D() => _reader.ReadByte() != 3
         ? Point3D.Zero
-        : new Point3D(m_Reader.ReadInt16(), m_Reader.ReadInt16(), m_Reader.ReadByte());
+        : new Point3D(_reader.ReadInt16(), _reader.ReadInt16(), _reader.ReadByte());
 
     public string ReadUnicodeStringSafe() =>
-        m_Reader.ReadByte() != 2 ? string.Empty : m_Reader.ReadBigUniSafe(m_Reader.ReadUInt16());
+        _reader.ReadByte() != 2 ? string.Empty : _reader.ReadBigUniSafe(_reader.ReadUInt16());
 
     public string ReadUnicodeString() =>
-        m_Reader.ReadByte() != 2 ? string.Empty : m_Reader.ReadBigUni(m_Reader.ReadUInt16());
+        _reader.ReadByte() != 2 ? string.Empty : _reader.ReadBigUni(_reader.ReadUInt16());
 }
