3aef20ae526db3ace67defef839bb1a7f0c76ab1
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-16 22:43:31 -0700

    WIP

--- Projects/UOContent/Items/Lights/BaseEquippableLight.cs
+++ Projects/UOContent/Items/Lights/BaseEquippableLight.cs
@@ -1,62 +1,64 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public abstract partial class BaseEquipableLight : BaseLight
 {
-    public abstract class BaseEquipableLight : BaseLight
+    [Constructible]
+    public BaseEquipableLight(int itemID) : base(itemID) => Layer = Layer.TwoHanded;
+
+    public BaseEquipableLight(Serial serial) : base(serial)
     {
-        [Constructible]
-        public BaseEquipableLight(int itemID) : base(itemID) => Layer = Layer.TwoHanded;
+    }
 
-        public BaseEquipableLight(Serial serial) : base(serial)
+    public override void Ignite()
+    {
+        if (Parent is not Mobile && RootParent is Mobile holder)
         {
-        }
-
-        public override void Ignite()
-        {
-            if (Parent is not Mobile && RootParent is Mobile holder)
+            if (holder.EquipItem(this))
             {
-                if (holder.EquipItem(this))
+                if (this is Candle)
                 {
-                    if (this is Candle)
-                    {
-                        holder.SendLocalizedMessage(502969); // You put the candle in your left hand.
-                    }
-                    else if (this is Torch)
-                    {
-                        holder.SendLocalizedMessage(502971); // You put the torch in your left hand.
-                    }
+                    holder.SendLocalizedMessage(502969); // You put the candle in your left hand.
+                }
+                else if (this is Torch)
+                {
+                    holder.SendLocalizedMessage(502971); // You put the torch in your left hand.
+                }
 
-                    base.Ignite();
-                }
-                else
-                {
-                    holder.SendLocalizedMessage(502449); // You cannot hold this item.
-                }
+                base.Ignite();
             }
             else
             {
-                base.Ignite();
+                holder.SendLocalizedMessage(502449); // You cannot hold this item.
             }
         }
-
-        public override void OnAdded(IEntity parent)
+        else
         {
-            if (Burning && parent is Container)
-            {
-                Douse();
-            }
+            base.Ignite();
+        }
+    }
 
-            base.OnAdded(parent);
+    public override void OnAdded(IEntity parent)
+    {
+        if (Burning && parent is Container)
+        {
+            Douse();
         }
 
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
+        base.OnAdded(parent);
+    }
 
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
+    public override void Serialize(IGenericWriter writer)
+    {
+        base.Serialize(writer);
+        writer.Write(0);
+    }
+
+    public override void Deserialize(IGenericReader reader)
+    {
+        base.Deserialize(reader);
+        var version = reader.ReadInt();
     }
 }
