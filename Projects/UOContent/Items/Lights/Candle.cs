666e1b92a68b21b494e59cafa53d6a50d7a04682
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-18 08:39:17 -0700

    fix: Codegens torches, candles, and lanterns

--- Projects/UOContent/Items/Lights/Candle.cs
+++ Projects/UOContent/Items/Lights/Candle.cs
@@ -1,37 +1,22 @@
 using System;
+using ModernUO.Serialization;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class Candle : BaseEquipableLight
 {
-    public class Candle : BaseEquipableLight
+    [Constructible]
+    public Candle() : base(0xA28)
     {
-        [Constructible]
-        public Candle() : base(0xA28)
-        {
-            Duration = Burnout ? TimeSpan.FromMinutes(20) : TimeSpan.Zero;
-            Burning = false;
+        Duration = Burnout ? TimeSpan.FromMinutes(20) : TimeSpan.Zero;
+        Burning = false;
 
-            Stackable = true;
-            Light = LightType.Circle150;
-            Weight = 1.0;
-        }
-
-        public Candle(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LitItemID => 0xA0F;
-        public override int UnlitItemID => 0xA28;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
-        }
+        Stackable = true;
+        Light = LightType.Circle150;
+        Weight = 1.0;
     }
+
+    public override int LitItemID => 0xA0F;
+    public override int UnlitItemID => 0xA28;
 }
