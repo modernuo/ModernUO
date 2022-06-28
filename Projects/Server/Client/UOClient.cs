4dbccc3f00982827837d203eafc39aeb5d9153c4
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-06-28 01:04:02 -0700

    Reverts

--- Projects/Server/Client/UOClient.cs
+++ Projects/Server/Client/UOClient.cs
@@ -131,8 +131,5 @@ public static class UOClient
 
         [JsonPropertyName("ultimaonlinedirectory")]
         public string UltimaOnlineDirectory { get; set; }
-
-        [JsonPropertyName("plugins")]
-        public string[] Plugins { get; set; }
     }
 }
