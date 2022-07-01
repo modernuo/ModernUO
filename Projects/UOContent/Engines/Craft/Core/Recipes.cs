a9f7f2b7e99cee22ec067d03999c93f17b08a073
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-07-01 11:11:37 -0700

    fix: Fixes LearnAllRecipes (#1104)

--- Projects/UOContent/Engines/Craft/Core/Recipes.cs
+++ Projects/UOContent/Engines/Craft/Core/Recipes.cs
@@ -3,99 +3,96 @@ using System.Collections.Generic;
 using Server.Mobiles;
 using Server.Targeting;
 
-namespace Server.Engines.Craft
+namespace Server.Engines.Craft;
+
+public class Recipe
 {
-    public class Recipe
+    private TextDefinition _td;
+
+    public Recipe(int id, CraftSystem system, CraftItem item)
     {
-        private TextDefinition m_TD;
+        ID = id;
+        CraftSystem = system;
+        CraftItem = item;
 
-        public Recipe(int id, CraftSystem system, CraftItem item)
+        if (!Recipes.TryAdd(id, this))
         {
-            ID = id;
-            CraftSystem = system;
-            CraftItem = item;
+            throw new Exception("Attempting to create recipe with preexisting ID.");
+        }
 
-            if (Recipes.ContainsKey(id))
+        LargestRecipeID = Math.Max(id, LargestRecipeID);
+    }
+
+    public static Dictionary<int, Recipe> Recipes { get; } = new();
+
+    public static int LargestRecipeID { get; private set; }
+
+    public CraftSystem CraftSystem { get; set; }
+
+    public CraftItem CraftItem { get; set; }
+
+    public int ID { get; }
+
+    public TextDefinition TextDefinition => _td ??= new TextDefinition(CraftItem.NameNumber, CraftItem.NameString);
+
+    public static void Initialize()
+    {
+        CommandSystem.Register("LearnAllRecipes", AccessLevel.GameMaster, LearnAllRecipes_OnCommand);
+        CommandSystem.Register("ForgetAllRecipes", AccessLevel.GameMaster, ForgetAllRecipes_OnCommand);
+    }
+
+    [Usage("LearnAllRecipes"), Description("Teaches a player all available recipes.")]
+    private static void LearnAllRecipes_OnCommand(CommandEventArgs e)
+    {
+        var m = e.Mobile;
+        m.SendMessage("Target a player to teach them all of the recipes.");
+
+        m.BeginTarget(
+            -1,
+            false,
+            TargetFlags.None,
+            (from, targeted) =>
             {
-                throw new Exception("Attempting to create recipe with preexisting ID.");
+                if (targeted is PlayerMobile mobile)
+                {
+                    foreach (var kvp in Recipes)
+                    {
+                        mobile.AcquireRecipe(kvp.Key);
+                    }
+
+                    from.SendMessage("You teach them all of the recipes.");
+                }
+                else
+                {
+                    from.SendMessage("That is not a player!");
+                }
             }
+        );
+    }
 
-            Recipes.Add(id, this);
-            LargestRecipeID = Math.Max(id, LargestRecipeID);
-        }
+    [Usage("ForgetAllRecipes"), Description("Makes a player forget all the recipes they've learned.")]
+    private static void ForgetAllRecipes_OnCommand(CommandEventArgs e)
+    {
+        var m = e.Mobile;
+        m.SendMessage("Target a player to have them forget all of the recipes they've learned.");
 
-        public static Dictionary<int, Recipe> Recipes { get; } = new();
-
-        public static int LargestRecipeID { get; private set; }
-
-        public CraftSystem CraftSystem { get; set; }
-
-        public CraftItem CraftItem { get; set; }
-
-        public int ID { get; }
-
-        public TextDefinition TextDefinition =>
-            m_TD ?? (m_TD = new TextDefinition(CraftItem.NameNumber, CraftItem.NameString));
-
-        public static void Initialize()
-        {
-            CommandSystem.Register("LearnAllRecipes", AccessLevel.GameMaster, LearnAllRecipes_OnCommand);
-            CommandSystem.Register("ForgetAllRecipes", AccessLevel.GameMaster, ForgetAllRecipes_OnCommand);
-        }
-
-        [Usage("LearnAllRecipes"), Description("Teaches a player all available recipes.")]
-        private static void LearnAllRecipes_OnCommand(CommandEventArgs e)
-        {
-            var m = e.Mobile;
-            m.SendMessage("Target a player to teach them all of the recipes.");
-
-            m.BeginTarget(
-                -1,
-                false,
-                TargetFlags.None,
-                (from, targeted) =>
+        m.BeginTarget(
+            -1,
+            false,
+            TargetFlags.None,
+            (from, targeted) =>
+            {
+                if (targeted is PlayerMobile mobile)
                 {
-                    if (targeted is PlayerMobile mobile)
-                    {
-                        foreach (var kvp in Recipes)
-                        {
-                            mobile.AcquireRecipe(kvp.Key);
-                        }
+                    mobile.ResetRecipes();
 
-                        from.SendMessage("You teach them all of the recipes.");
-                    }
-                    else
-                    {
-                        from.SendMessage("That is not a player!");
-                    }
+                    from.SendMessage("They forget all their recipes.");
                 }
-            );
-        }
-
-        [Usage("ForgetAllRecipes"), Description("Makes a player forget all the recipes they've learned.")]
-        private static void ForgetAllRecipes_OnCommand(CommandEventArgs e)
-        {
-            var m = e.Mobile;
-            m.SendMessage("Target a player to have them forget all of the recipes they've learned.");
-
-            m.BeginTarget(
-                -1,
-                false,
-                TargetFlags.None,
-                (from, targeted) =>
+                else
                 {
-                    if (targeted is PlayerMobile mobile)
-                    {
-                        mobile.ResetRecipes();
-
-                        from.SendMessage("They forget all their recipes.");
-                    }
-                    else
-                    {
-                        from.SendMessage("That is not a player!");
-                    }
+                    from.SendMessage("That is not a player!");
                 }
-            );
-        }
+            }
+        );
     }
 }
