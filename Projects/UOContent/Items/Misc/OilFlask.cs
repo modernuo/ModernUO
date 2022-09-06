6e9a4c60e53a1b1a6df8445613572067cebdea68
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-09-06 14:26:41 -0700

    Update Projects/UOContent/Items/Misc/OilFlask.cs

--- Projects/UOContent/Items/Misc/OilFlask.cs
+++ Projects/UOContent/Items/Misc/OilFlask.cs
@@ -73,7 +73,9 @@ public partial class OilFlask : Item
                 eable.Free();
 
                 if (!didStack)
+                {
                     emptyFlask.MoveToWorld(Location, Map);
+                }
             }
 
             Consume(1);
