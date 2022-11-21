3de9867569226f0c7c53a038c0d699cb52a69801
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-11-21 10:30:41 -0800

    Fixes copyright

--- Projects/Server/Regions/RegionJsonSerializer.cs
+++ Projects/Server/Regions/RegionJsonSerializer.cs
@@ -2,7 +2,7 @@
  * ModernUO                                                              *
  * Copyright 2019-2022 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
- * File: RegionSerializer.cs                                             *
+ * File: RegionJsonSerializer.cs                                         *
  *                                                                       *
  * This program is free software: you can redistribute it and/or modify  *
  * it under the terms of the GNU General Public License as published by  *
