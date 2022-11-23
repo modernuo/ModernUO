2bd144e368dca25c51319a68c3c4caacd6943e07
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-11-23 13:35:47 -0800

    fix: Codegens quivers

--- Projects/UOContent/Items/Quivers/QuiverOfBlight.cs
+++ Projects/UOContent/Items/Quivers/QuiverOfBlight.cs
@@ -1,37 +1,21 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class QuiverOfBlight : ElvenQuiver
 {
-    public class QuiverOfBlight : ElvenQuiver
+    [Constructible]
+    public QuiverOfBlight() => Hue = 0x4F3;
+
+    public override int LabelNumber => 1073111; // Quiver of Blight
+
+    public override void AlterBowDamage(
+        ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
+        ref int chaos, ref int direct
+    )
     {
-        [Constructible]
-        public QuiverOfBlight() => Hue = 0x4F3;
-
-        public QuiverOfBlight(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1073111; // Quiver of Blight
-
-        public override void AlterBowDamage(
-            ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
-            ref int chaos, ref int direct
-        )
-        {
-            phys = fire = nrgy = chaos = direct = 0;
-            cold = pois = 50;
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        phys = fire = nrgy = chaos = direct = 0;
+        cold = pois = 50;
     }
 }
--- Projects/UOContent/Items/Quivers/QuiverOfLightning.cs
+++ Projects/UOContent/Items/Quivers/QuiverOfLightning.cs
@@ -1,37 +1,21 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class QuiverOfLightning : ElvenQuiver
 {
-    public class QuiverOfLightning : ElvenQuiver
+    [Constructible]
+    public QuiverOfLightning() => Hue = 0x4F9;
+
+    public override int LabelNumber => 1073112; // Quiver of Lightning
+
+    public override void AlterBowDamage(
+        ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
+        ref int chaos, ref int direct
+    )
     {
-        [Constructible]
-        public QuiverOfLightning() => Hue = 0x4F9;
-
-        public QuiverOfLightning(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1073112; // Quiver of Lightning
-
-        public override void AlterBowDamage(
-            ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
-            ref int chaos, ref int direct
-        )
-        {
-            fire = cold = pois = chaos = direct = 0;
-            phys = nrgy = 50;
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        fire = cold = pois = chaos = direct = 0;
+        phys = nrgy = 50;
     }
 }
