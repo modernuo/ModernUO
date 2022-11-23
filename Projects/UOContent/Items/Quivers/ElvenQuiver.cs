59ed0a5bb4723e62aa9fff6a8b8e00510ed3f903
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-11-23 13:37:39 -0800

    fix: Codegens quivers (#1271)

--- Projects/UOContent/Items/Quivers/ElvenQuiver.cs
+++ Projects/UOContent/Items/Quivers/ElvenQuiver.cs
@@ -1,29 +1,13 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[Flippable(0x2FB7, 0x3171)]
+[SerializationGenerator(0)]
+public partial class ElvenQuiver : BaseQuiver
 {
-    [Flippable(0x2FB7, 0x3171)]
-    public class ElvenQuiver : BaseQuiver
-    {
-        [Constructible]
-        public ElvenQuiver() => WeightReduction = 30;
+    [Constructible]
+    public ElvenQuiver() => WeightReduction = 30;
 
-        public ElvenQuiver(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1032657; // elven quiver
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
-    }
+    public override int LabelNumber => 1032657; // elven quiver
 }
--- Projects/UOContent/Items/Quivers/QuiverOfIce.cs
+++ Projects/UOContent/Items/Quivers/QuiverOfIce.cs
@@ -1,37 +1,21 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class QuiverOfIce : ElvenQuiver
 {
-    public class QuiverOfIce : ElvenQuiver
+    [Constructible]
+    public QuiverOfIce() => Hue = 0x4ED;
+
+    public override int LabelNumber => 1073110; // quiver of ice
+
+    public override void AlterBowDamage(
+        ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
+        ref int chaos, ref int direct
+    )
     {
-        [Constructible]
-        public QuiverOfIce() => Hue = 0x4ED;
-
-        public QuiverOfIce(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1073110; // quiver of ice
-
-        public override void AlterBowDamage(
-            ref int phys, ref int fire, ref int cold, ref int pois, ref int nrgy,
-            ref int chaos, ref int direct
-        )
-        {
-            fire = pois = nrgy = chaos = direct = 0;
-            phys = cold = 50;
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        fire = pois = nrgy = chaos = direct = 0;
+        phys = cold = 50;
     }
 }
