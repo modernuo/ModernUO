af29fa0243b0949311310bd8a6ad5e77f43c005f
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-11-28 23:42:32 -0800

    fix: Codegens fishing pole (#1288)

--- Projects/UOContent/Items/Skill Items/Fishing/FishingPole.cs
+++ Projects/UOContent/Items/Skill Items/Fishing/FishingPole.cs
@@ -1,77 +1,55 @@
 using System.Collections.Generic;
+using ModernUO.Serialization;
 using Server.ContextMenus;
 using Server.Engines.Harvest;
 using Server.Network;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class FishingPole : Item
 {
-    public class FishingPole : Item
+    [Constructible]
+    public FishingPole() : base(0x0DC0)
     {
-        [Constructible]
-        public FishingPole() : base(0x0DC0)
+        Layer = Layer.TwoHanded;
+        Weight = 8.0;
+    }
+
+    public override void OnDoubleClick(Mobile from)
+    {
+        var loc = GetWorldLocation();
+
+        if (!from.InLOS(loc) || !from.InRange(loc, 2))
         {
-            Layer = Layer.TwoHanded;
-            Weight = 8.0;
+            from.LocalOverheadMessage(MessageType.Regular, 0x3E9, 1019045); // I can't reach that
+        }
+        else
+        {
+            Fishing.System.BeginHarvesting(from, this);
+        }
+    }
+
+    public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
+    {
+        base.GetContextMenuEntries(from, list);
+
+        BaseHarvestTool.AddContextMenuEntries(from, this, list, Fishing.System);
+    }
+
+    public override bool CheckConflictingLayer(Mobile m, Item item, Layer layer)
+    {
+        if (base.CheckConflictingLayer(m, item, layer))
+        {
+            return true;
         }
 
-        public FishingPole(Serial serial) : base(serial)
+        if (layer == Layer.OneHanded)
         {
+            m.SendLocalizedMessage(500214); // You already have something in both hands.
+            return true;
         }
 
-        public override void OnDoubleClick(Mobile from)
-        {
-            var loc = GetWorldLocation();
-
-            if (!from.InLOS(loc) || !from.InRange(loc, 2))
-            {
-                from.LocalOverheadMessage(MessageType.Regular, 0x3E9, 1019045); // I can't reach that
-            }
-            else
-            {
-                Fishing.System.BeginHarvesting(from, this);
-            }
-        }
-
-        public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
-        {
-            base.GetContextMenuEntries(from, list);
-
-            BaseHarvestTool.AddContextMenuEntries(from, this, list, Fishing.System);
-        }
-
-        public override bool CheckConflictingLayer(Mobile m, Item item, Layer layer)
-        {
-            if (base.CheckConflictingLayer(m, item, layer))
-            {
-                return true;
-            }
-
-            if (layer == Layer.OneHanded)
-            {
-                m.SendLocalizedMessage(500214); // You already have something in both hands.
-                return true;
-            }
-
-            return false;
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(1); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-
-            if (version < 1 && Layer == Layer.OneHanded)
-            {
-                Layer = Layer.TwoHanded;
-            }
-        }
+        return false;
     }
 }
