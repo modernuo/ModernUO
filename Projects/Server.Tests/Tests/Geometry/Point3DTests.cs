768f29f3fd1bf0cc360df85de231208cbe8fbfa6
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2022-12-07 20:10:08 -0800

    fix: Fixes Point3D TryParse

--- Projects/Server.Tests/Tests/Geometry/Point3DTests.cs
+++ Projects/Server.Tests/Tests/Geometry/Point3DTests.cs
@@ -5,6 +5,15 @@ namespace Server.Tests;
 
 public class Point3DTests
 {
+    [Theory]
+    [InlineData("(709, 2236, -2)", 709, 2236, -2)]
+    public void TestPoint3DRegressions(string text, int x, int y, int z)
+    {
+        var successful = Point3D.TryParse(text, null, out var p);
+        Assert.True(successful);
+        Assert.Equal(new Point3D(x, y, z), p);
+    }
+
     [Fact]
     public void TestPoint3DToString()
     {
@@ -124,5 +133,9 @@ public class Point3DTests
         Assert.False(Point3D.TryParse("(,23,)", null, out p));
         Assert.Equal(default, p);
         Assert.Throws<FormatException>(() => Point3D.Parse("(,23,)", null));
+
+        Assert.False(Point3D.TryParse("(23,,-1)", null, out p));
+        Assert.Equal(default, p);
+        Assert.Throws<FormatException>(() => Point3D.Parse("(23,,-1)", null));
     }
 }
