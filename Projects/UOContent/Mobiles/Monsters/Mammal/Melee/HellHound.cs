7017aefcf5341497f9bf83b6069233a0c93006cc
Arutosio <Arutosio@outlook.com>
2022-12-14 12:47:44 +0100

    fix: Changes on Mammal Monsters to use code generation
    Related Changes:
    - Deserialization adaptation:
        - Melee
            + VorpalBunny>BunnyHole

--- Projects/UOContent/Mobiles/Monsters/Mammal/Melee/HellHound.cs
+++ Projects/UOContent/Mobiles/Monsters/Mammal/Melee/HellHound.cs
@@ -1,8 +1,10 @@
+using ModernUO.Serialization;
 using Server.Items;
 
 namespace Server.Mobiles
 {
-    public class HellHound : BaseCreature
+    [SerializationGenerator(0, false)]
+    public partial class HellHound : BaseCreature
     {
         [Constructible]
         public HellHound() : base(AIType.AI_Melee)
@@ -38,10 +40,6 @@ namespace Server.Mobiles
             PackItem(new SulfurousAsh(5));
         }
 
-        public HellHound(Serial serial) : base(serial)
-        {
-        }
-
         public override string CorpseName => "a hell hound corpse";
         public override string DefaultName => "a hell hound";
         public override int Meat => 1;
@@ -55,18 +53,6 @@ namespace Server.Mobiles
         {
             AddLoot(LootPack.Average);
             AddLoot(LootPack.Meager);
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            var version = reader.ReadInt();
         }
     }
 }
