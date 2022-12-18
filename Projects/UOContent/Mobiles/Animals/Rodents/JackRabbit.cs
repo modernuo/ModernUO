f542ceded9c22f5fd239f4103a30bcafbcb19570
Arutosio <Arutosio@outlook.com>
2022-12-18 16:41:46 +0100

    fix: Changes on Rodents to use code generation

--- Projects/UOContent/Mobiles/Animals/Rodents/JackRabbit.cs
+++ Projects/UOContent/Mobiles/Animals/Rodents/JackRabbit.cs
@@ -1,7 +1,10 @@
+using ModernUO.Serialization;
+
 namespace Server.Mobiles
 {
     [TypeAlias("Server.Mobiles.Jackrabbit")]
-    public class JackRabbit : BaseCreature
+    [SerializationGenerator(0, false)]
+    public partial class JackRabbit : BaseCreature
     {
         [Constructible]
         public JackRabbit() : base(AIType.AI_Animal, FightMode.Aggressor)
@@ -36,10 +39,6 @@ namespace Server.Mobiles
             MinTameSkill = -18.9;
         }
 
-        public JackRabbit(Serial serial) : base(serial)
-        {
-        }
-
         public override string CorpseName => "a jack rabbit corpse";
         public override string DefaultName => "a jack rabbit";
 
@@ -52,19 +51,5 @@ namespace Server.Mobiles
         public override int GetHurtSound() => 0xCA;
 
         public override int GetDeathSound() => 0xCB;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-        }
     }
 }
--- Projects/UOContent/Mobiles/Animals/Rodents/SewerRat.cs
+++ Projects/UOContent/Mobiles/Animals/Rodents/SewerRat.cs
@@ -1,7 +1,10 @@
+using ModernUO.Serialization;
+
 namespace Server.Mobiles
 {
     [TypeAlias("Server.Mobiles.Sewerrat")]
-    public class SewerRat : BaseCreature
+    [SerializationGenerator(0, false)]
+    public partial class SewerRat : BaseCreature
     {
         [Constructible]
         public SewerRat() : base(AIType.AI_Melee)
@@ -38,10 +41,6 @@ namespace Server.Mobiles
             MinTameSkill = -0.9;
         }
 
-        public SewerRat(Serial serial) : base(serial)
-        {
-        }
-
         public override string CorpseName => "a rat corpse";
         public override string DefaultName => "a sewer rat";
 
@@ -51,20 +50,6 @@ namespace Server.Mobiles
         public override void GenerateLoot()
         {
             AddLoot(LootPack.Poor);
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
         }
     }
 }
