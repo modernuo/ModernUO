10b26741c71115b7c22493ff17aa9aa517bcb0e5
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-02-24 19:04:21 -0800

    fix: Codegens spellbooks (#1349)

--- Projects/UOContent/Items/Skill Items/Magical/BookOfBushido.cs
+++ Projects/UOContent/Items/Skill Items/Magical/BookOfBushido.cs
@@ -1,36 +1,15 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class BookOfBushido : Spellbook
 {
-    public class BookOfBushido : Spellbook
-    {
-        [Constructible]
-        public BookOfBushido(ulong content = 0x3F) : base(content, 0x238C) =>
-            Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
+    [Constructible]
+    public BookOfBushido(ulong content = 0x3F) : base(content, 0x238C) =>
+        Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
 
-        public BookOfBushido(Serial serial) : base(serial)
-        {
-        }
-
-        public override SpellbookType SpellbookType => SpellbookType.Samurai;
-        public override int BookOffset => 400;
-        public override int BookCount => 6;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(1); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-
-            if (version == 0 && Core.ML)
-            {
-                Layer = Layer.OneHanded;
-            }
-        }
-    }
+    public override SpellbookType SpellbookType => SpellbookType.Samurai;
+    public override int BookOffset => 400;
+    public override int BookCount => 6;
 }
--- Projects/UOContent/Items/Skill Items/Magical/MysticSpellbook.cs
+++ Projects/UOContent/Items/Skill Items/Magical/MysticSpellbook.cs
@@ -1,35 +1,15 @@
-﻿namespace Server.Items
+﻿using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class MysticSpellbook : Spellbook
 {
-    public class MysticSpellbook : Spellbook
-    {
-        [Constructible]
-        public MysticSpellbook(ulong content = 0)
-            : base(content, 0x2D9D) =>
-            Layer = Layer.OneHanded;
+    [Constructible]
+    public MysticSpellbook(ulong content = 0) : base(content, 0x2D9D) => Layer = Layer.OneHanded;
 
-        public MysticSpellbook(Serial serial)
-            : base(serial)
-        {
-        }
+    public override SpellbookType SpellbookType => SpellbookType.Mystic;
 
-        public override SpellbookType SpellbookType => SpellbookType.Mystic;
-
-        public override int BookOffset => 677;
-        public override int BookCount => 16;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            /*int version = */
-            reader.ReadInt();
-        }
-    }
+    public override int BookOffset => 677;
+    public override int BookCount => 16;
 }
