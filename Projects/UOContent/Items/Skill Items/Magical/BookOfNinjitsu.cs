6ec5223ca1e45d26370c9bfc27aa718a460a6a1a
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-02-24 19:03:05 -0800

    fix: Codegens spellbooks

--- Projects/UOContent/Items/Skill Items/Magical/BookOfNinjitsu.cs
+++ Projects/UOContent/Items/Skill Items/Magical/BookOfNinjitsu.cs
@@ -1,36 +1,15 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class BookOfNinjitsu : Spellbook
 {
-    public class BookOfNinjitsu : Spellbook
-    {
-        [Constructible]
-        public BookOfNinjitsu(ulong content = 0xFF) : base(content, 0x23A0) =>
-            Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
+    [Constructible]
+    public BookOfNinjitsu(ulong content = 0xFF) : base(content, 0x23A0) =>
+        Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
 
-        public BookOfNinjitsu(Serial serial) : base(serial)
-        {
-        }
-
-        public override SpellbookType SpellbookType => SpellbookType.Ninja;
-        public override int BookOffset => 500;
-        public override int BookCount => 8;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(1); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-
-            if (version == 0 && Core.ML)
-            {
-                Layer = Layer.OneHanded;
-            }
-        }
-    }
+    public override SpellbookType SpellbookType => SpellbookType.Ninja;
+    public override int BookOffset => 500;
+    public override int BookCount => 8;
 }
--- Projects/UOContent/Items/Skill Items/Magical/NecromancerSpellbook.cs
+++ Projects/UOContent/Items/Skill Items/Magical/NecromancerSpellbook.cs
@@ -1,36 +1,15 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class NecromancerSpellbook : Spellbook
 {
-    public class NecromancerSpellbook : Spellbook
-    {
-        [Constructible]
-        public NecromancerSpellbook(ulong content = 0) : base(content, 0x2253) =>
-            Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
+    [Constructible]
+    public NecromancerSpellbook(ulong content = 0) : base(content, 0x2253) =>
+        Layer = Core.ML ? Layer.OneHanded : Layer.Invalid;
 
-        public NecromancerSpellbook(Serial serial) : base(serial)
-        {
-        }
-
-        public override SpellbookType SpellbookType => SpellbookType.Necromancer;
-        public override int BookOffset => 100;
-        public override int BookCount => Core.SE ? 17 : 16;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.Write(1); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadInt();
-
-            if (version == 0 && Core.ML)
-            {
-                Layer = Layer.OneHanded;
-            }
-        }
-    }
+    public override SpellbookType SpellbookType => SpellbookType.Necromancer;
+    public override int BookOffset => 100;
+    public override int BookCount => Core.SE ? 17 : 16;
 }
