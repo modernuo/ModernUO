5ac214b8c21bf22541b51c0e25dcf2cda4f20b50
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-03-07 21:07:18 -0800

    fix: Codegens gifts

--- Projects/UOContent/Items/Special/Gifts/SamuraiHelm.cs
+++ Projects/UOContent/Items/Special/Gifts/SamuraiHelm.cs
@@ -1,49 +1,33 @@
-namespace Server.Items
+using ModernUO.Serialization;
+
+namespace Server.Items;
+
+[Flippable(0x236C, 0x236D)]
+[SerializationGenerator(0)]
+public partial class SamuraiHelm : BaseArmor
 {
-    [Flippable(0x236C, 0x236D)]
-    public class SamuraiHelm : BaseArmor
+    [Constructible]
+    public SamuraiHelm() : base(0x236C)
     {
-        [Constructible]
-        public SamuraiHelm() : base(0x236C)
-        {
-            Weight = 5.0;
-            LootType = LootType.Blessed;
+        Weight = 5.0;
+        LootType = LootType.Blessed;
 
-            Attributes.DefendChance = 15;
-            ArmorAttributes.SelfRepair = 10;
-            ArmorAttributes.LowerStatReq = 100;
-            ArmorAttributes.MageArmor = 1;
-        }
-
-        public SamuraiHelm(Serial serial) : base(serial)
-        {
-        }
-
-        public override int LabelNumber => 1062923; // Ancient Samurai Helm
-
-        public override int BasePhysicalResistance => 15;
-        public override int BaseFireResistance => 10;
-        public override int BaseColdResistance => 10;
-        public override int BasePoisonResistance => 15;
-        public override int BaseEnergyResistance => 10;
-
-        public override int InitMinHits => 255;
-        public override int InitMaxHits => 255;
-
-        public override ArmorMaterialType MaterialType => ArmorMaterialType.Plate;
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        Attributes.DefendChance = 15;
+        ArmorAttributes.SelfRepair = 10;
+        ArmorAttributes.LowerStatReq = 100;
+        ArmorAttributes.MageArmor = 1;
     }
+
+    public override int LabelNumber => 1062923; // Ancient Samurai Helm
+
+    public override int BasePhysicalResistance => 15;
+    public override int BaseFireResistance => 10;
+    public override int BaseColdResistance => 10;
+    public override int BasePoisonResistance => 15;
+    public override int BaseEnergyResistance => 10;
+
+    public override int InitMinHits => 255;
+    public override int InitMaxHits => 255;
+
+    public override ArmorMaterialType MaterialType => ArmorMaterialType.Plate;
 }
