5ce657a842254ff3adb9b2f3f72349fd623de13e
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-04-05 09:21:44 -0700

    fix: Fixes speech throttle (#1387)

--- Projects/UOContent/Misc/PacketThrottles.cs
+++ Projects/UOContent/Misc/PacketThrottles.cs
@@ -141,7 +141,7 @@ namespace Server.Network
                 return true;
             }
 
-            if (Core.TickCount - ns.GetPacketTime(packetID) + Delays[packetID] > 0)
+            if (Core.TickCount - ns.GetPacketTime(packetID) < Delays[packetID])
             {
                 drop = true;
                 return false;
