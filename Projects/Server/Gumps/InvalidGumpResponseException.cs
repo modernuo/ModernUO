8389bfacfe71a9395b7ec147daa26d1f795b7e70
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-08-09 09:09:26 -0700

    chore: Updates copyright (#1448)

--- Projects/Server/Gumps/InvalidGumpResponseException.cs
+++ Projects/Server/Gumps/InvalidGumpResponseException.cs
@@ -1,6 +1,6 @@
 /*************************************************************************
  * ModernUO                                                              *
- * Copyright (C) 2019-2022 - ModernUO Development Team                   *
+ * Copyright 2019-2023 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
  * File: InvalidGumpResponseException.cs                                 *
  *                                                                       *
--- Projects/Server/Random/WeightedValue.cs
+++ Projects/Server/Random/WeightedValue.cs
@@ -1,6 +1,6 @@
 ﻿/*************************************************************************
  * ModernUO                                                              *
- * Copyright 2019-2022 - ModernUO Development Team                       *
+ * Copyright 2019-2023 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
  * File: WeightedValue.cs                                                *
  *                                                                       *
