f56cb31db6302e1deef289ccc315e3f29a4d22be
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2023-08-23 20:02:46 -0700

    fix: Codegens rest of special items (#1467)

--- Projects/UOContent/Items/Special/HeritageToken.cs
+++ Projects/UOContent/Items/Special/HeritageToken.cs
@@ -1,54 +1,37 @@
-﻿using Server.Gumps;
+﻿using ModernUO.Serialization;
+using Server.Gumps;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class HeritageToken : Item
 {
-    public class HeritageToken : Item
+    [Constructible]
+    public HeritageToken() : base(0x367A)
     {
-        [Constructible]
-        public HeritageToken() : base(0x367A)
+        LootType = LootType.Blessed;
+        Weight = 5.0;
+    }
+
+    public override int LabelNumber => 1076596; // A Heritage Token
+
+    public override void OnDoubleClick(Mobile from)
+    {
+        if (IsChildOf(from.Backpack))
         {
-            LootType = LootType.Blessed;
-            Weight = 5.0;
+            from.CloseGump<HeritageTokenGump>();
+            from.SendGump(new HeritageTokenGump(this));
         }
-
-        public HeritageToken(Serial serial) : base(serial)
+        else
         {
+            from.SendLocalizedMessage(1062334); // This item must be in your backpack to be used.
         }
+    }
 
-        public override int LabelNumber => 1076596; // A Heritage Token
+    public override void GetProperties(IPropertyList list)
+    {
+        base.GetProperties(list);
 
-        public override void OnDoubleClick(Mobile from)
-        {
-            if (IsChildOf(from.Backpack))
-            {
-                from.CloseGump<HeritageTokenGump>();
-                from.SendGump(new HeritageTokenGump(this));
-            }
-            else
-            {
-                from.SendLocalizedMessage(1062334); // This item must be in your backpack to be used.
-            }
-        }
-
-        public override void GetProperties(IPropertyList list)
-        {
-            base.GetProperties(list);
-
-            list.AddLocalized(1070998, 1076595); // Use this to redeem<br>Your Heritage Items
-        }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-
-            writer.WriteEncodedInt(0); // version
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-
-            var version = reader.ReadEncodedInt();
-        }
+        list.AddLocalized(1070998, 1076595); // Use this to redeem<br>Your Heritage Items
     }
 }
