e680bdb4b5d160362c3826a8a400ffc7b2119686
mdodkins <matthew@spaceshipaudio.co.uk>
2023-11-21 20:21:59 +0000

    fix: Adds boolean support for Server.TryParse (#1605)

---
+++ Projects/UOContent.Tests/Tests/Utilities/TryParseTests.cs
@@ -0,0 +1,23 @@
+using Server;
+using System;
+using Xunit;
+
+namespace Server.Tests.Utility;
+
+public class TryParseTests
+{
+    [Theory]
+    [InlineData("True", null, true)]
+    [InlineData("False", null, false)]
+    [InlineData("Alakazam", "Not a valid boolean string.", true)]
+    public void TestTryParseBool(string value, string returned, bool parsedAs)
+    {
+        string actualReturned = Server.Types.TryParse(typeof(bool), value, out object constructed);
+        Assert.Equal(returned, actualReturned);
+
+        if (returned == null)
+        {
+            Assert.Equal(parsedAs, constructed);
+        }
+    }
+}
