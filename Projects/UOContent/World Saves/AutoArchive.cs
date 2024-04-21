94c1be46c341df7b9e6a47a5f17ff746c9e73dc0
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-04-21 08:07:39 -0700

    fix: Archive locally is not an async function

--- Projects/UOContent/World Saves/AutoArchive.cs
+++ Projects/UOContent/World Saves/AutoArchive.cs
@@ -218,7 +218,7 @@ namespace Server.Saves
             }
         }
 
-        public static async void AutoArchiveLocally()
+        public static void AutoArchiveLocally()
         {
             var date = Core.Now;
 
