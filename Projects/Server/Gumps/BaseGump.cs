9c99ba124ac42e32d420fdf9fa3c010f5f502258
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-04-25 22:28:39 -0700

    Fix copyright

--- Projects/Server/Gumps/BaseGump.cs
+++ Projects/Server/Gumps/BaseGump.cs
@@ -1,8 +1,8 @@
 /*************************************************************************
  * ModernUO                                                              *
- * Copyright 2019-2023 - ModernUO Development Team                       *
+ * Copyright 2019-2024 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
- * File: BaseGump.cs                                             *
+ * File: BaseGump.cs                                                     *
  *                                                                       *
  * This program is free software: you can redistribute it and/or modify  *
  * it under the terms of the GNU General Public License as published by  *
