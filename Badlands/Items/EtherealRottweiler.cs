23f4979e0cc35e0284dc47d887245d5867503bcb
John Scott <reeeetus@gmail.com>
2024-05-19 14:53:58 +0700

    EtherealRottweiler

---
+++ Badlands/Items/EtherealRottweiler.cs
@@ -0,0 +1,31 @@
+// Copyright (C) 2024 Reetus
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using ModernUO.Serialization;
+using Server;
+using Server.Mobiles;
+
+namespace Badlands.Items;
+
+[SerializationGenerator( 0, false )]
+public partial class EtherealRottweiler : EtherealMount
+{
+    [Constructible]
+    public EtherealRottweiler() : base( 0xA770, 0x3ED9 )
+    {
+    }
+
+    public override string DefaultName => "an ethereal rottweiler";
+}
