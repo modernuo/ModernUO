8e4177570dc2780772d3052666b3052b8b524697
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-06-03 17:05:48 -0700

    fix: Converts ethics system to generic entity persistence

--- Projects/UOContent/Engines/Ethics/Core/Power.cs
+++ Projects/UOContent/Engines/Ethics/Core/Power.cs
@@ -1,37 +1,36 @@
-namespace Server.Ethics
+namespace Server.Ethics;
+
+public abstract class Power
 {
-    public abstract class Power
+    protected PowerDefinition m_Definition;
+
+    public PowerDefinition Definition => m_Definition;
+
+    public virtual bool CheckInvoke(Player from)
     {
-        protected PowerDefinition m_Definition;
-
-        public PowerDefinition Definition => m_Definition;
-
-        public virtual bool CheckInvoke(Player from)
+        if (!from.Mobile.CheckAlive())
         {
-            if (!from.Mobile.CheckAlive())
-            {
-                return false;
-            }
-
-            if (from.Power < m_Definition.Power)
-            {
-                from.Mobile.LocalOverheadMessage(
-                    MessageType.Regular,
-                    0x3B2,
-                    false,
-                    "You lack the power to invoke this ability."
-                );
-                return false;
-            }
-
-            return true;
+            return false;
         }
 
-        public abstract void BeginInvoke(Player from);
-
-        public virtual void FinishInvoke(Player from)
+        if (from.Power < m_Definition.Power)
         {
-            from.Power -= m_Definition.Power;
+            from.Mobile.LocalOverheadMessage(
+                MessageType.Regular,
+                0x3B2,
+                false,
+                "You lack the power to invoke this ability."
+            );
+            return false;
         }
+
+        return true;
+    }
+
+    public abstract void BeginInvoke(Player from);
+
+    public virtual void FinishInvoke(Player from)
+    {
+        from.Power -= m_Definition.Power;
     }
 }
