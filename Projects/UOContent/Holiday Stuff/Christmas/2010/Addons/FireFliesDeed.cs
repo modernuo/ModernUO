85dcf3f1271304864dbd81619d09a56a35a62f5e
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-07-23 20:30:09 -0700

    fix: Removes wrong error message from FireFlies

--- Projects/UOContent/Holiday Stuff/Christmas/2010/Addons/FireFliesDeed.cs
+++ Projects/UOContent/Holiday Stuff/Christmas/2010/Addons/FireFliesDeed.cs
@@ -4,244 +4,222 @@ using Server.Multis;
 using Server.Network;
 using Server.Targeting;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0)]
+public partial class Fireflies : Item, IAddon
 {
-    [SerializationGenerator(0)]
-    public partial class Fireflies : Item, IAddon
+    [Constructible]
+    public Fireflies(int itemID = 0x1596) : base(itemID)
     {
-        [Constructible]
-        public Fireflies(int itemID = 0x1596)
-            : base(itemID)
+        LootType = LootType.Blessed;
+        Movable = false;
+    }
+
+    public override int LabelNumber => 1150061;
+
+    public bool FacingSouth => ItemID == 0x2336;
+
+    public Item Deed => new FirefliesDeed();
+
+    public bool CouldFit(IPoint3D p, Map map)
+    {
+        if (map?.CanFit(p.X, p.Y, p.Z, ItemData.Height) != true)
         {
-            LootType = LootType.Blessed;
-            Movable = false;
+            return false;
         }
 
-        public override int LabelNumber => 1150061;
+        return FacingSouth
+            ? BaseAddon.IsWall(p.X, p.Y - 1, p.Z, map)
+            : BaseAddon.IsWall(p.X - 1, p.Y, p.Z, map);
+    }
 
-        public bool FacingSouth => ItemID == 0x2336;
-
-        public Item Deed => new FirefliesDeed();
-
-        public bool CouldFit(IPoint3D p, Map map)
+    public override void OnDoubleClick(Mobile from)
+    {
+        if (!from.InRange(Location, 3))
         {
-            if (map?.CanFit(p.X, p.Y, p.Z, ItemData.Height) != true)
-            {
-                return false;
-            }
-
-            return FacingSouth ?
-                BaseAddon.IsWall(p.X, p.Y - 1, p.Z, map) :
-                BaseAddon.IsWall(p.X - 1, p.Y, p.Z, map);
+            from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+            return;
         }
 
-        public override void OnDoubleClick(Mobile from)
-        {
-            if (from.InRange(Location, 3))
-            {
-                var house = BaseHouse.FindHouseAt(this);
+        var house = BaseHouse.FindHouseAt(this);
 
-                if (house?.IsOwner(from) == true)
-                {
-                    from.CloseGump<RewardDemolitionGump>();
-                    from.SendGump(new RewardDemolitionGump(this, 1049783)); // Do you wish to re-deed this decoration?
-                }
-                else
-                {
-                    from.SendLocalizedMessage(
-                        1049784
-                    ); // You can only re-deed this decoration if you are the house owner or originally placed the decoration.
-                }
-            }
-            else
+        if (house?.IsOwner(from) == true)
+        {
+            from.CloseGump<RewardDemolitionGump>();
+            // Do you wish to re-deed this decoration?
+            from.SendGump(new RewardDemolitionGump(this, 1049783));
+        }
+        else
+        {
+            // You can only re-deed this decoration if you are the house owner or originally placed the decoration.
+            from.SendLocalizedMessage(1049784);
+        }
+    }
+}
+
+[SerializationGenerator(0)]
+public partial class FirefliesDeed : Item
+{
+    [Constructible]
+    public FirefliesDeed() : base(0x14F0)
+    {
+        LootType = LootType.Blessed;
+        Weight = 1.0;
+    }
+
+    public override int LabelNumber => 1150061;
+
+    public override void OnDoubleClick(Mobile from)
+    {
+        if (!IsChildOf(from.Backpack))
+        {
+            from.SendLocalizedMessage(1042038); // You must have the object in your backpack to use it.
+            return;
+        }
+
+        var house = BaseHouse.FindHouseAt(from);
+
+        if (house?.IsOwner(from) != true)
+        {
+            from.SendLocalizedMessage(502092); // You must be in your house to do this.
+            return;
+        }
+
+        from.CloseGump<FacingGump>();
+        from.SendGump(new FacingGump(this, from));
+    }
+
+    private class FacingGump : Gump
+    {
+        private readonly FirefliesDeed _deed;
+        private readonly Mobile _placer;
+
+        public FacingGump(FirefliesDeed deed, Mobile player) : base(150, 50)
+        {
+            _deed = deed;
+            _placer = player;
+
+            Closable = true;
+            Disposable = true;
+            Draggable = true;
+            Resizable = false;
+
+            AddPage(0);
+
+            AddBackground(0, 0, 300, 150, 0xA28);
+
+            AddItem(90, 30, 0x2332);
+            AddItem(180, 30, 0x2336);
+
+            AddButton(50, 35, 0x868, 0x869, (int)Buttons.East);
+            AddButton(145, 35, 0x868, 0x869, (int)Buttons.South);
+        }
+
+        public override void OnResponse(NetState sender, in RelayInfo info)
+        {
+            if (info.ButtonID == (int)Buttons.Cancel)
             {
-                from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1019045); // I can't reach that.
+                return;
             }
+
+            int itemId = info.ButtonID switch
+            {
+                (int)Buttons.East  => 0x2332,
+                (int)Buttons.South => 0x2336,
+                _                  => 0
+            };
+
+            _placer.Target = new InternalTarget(_deed, itemId);
+        }
+
+        private enum Buttons
+        {
+            Cancel,
+            South,
+            East
         }
     }
 
-    [SerializationGenerator(0)]
-    public partial class FirefliesDeed : Item
+    private class InternalTarget : Target
     {
-        [Constructible]
-        public FirefliesDeed()
-            : base(0x14F0)
+        private readonly FirefliesDeed _firefliesDeed;
+        private readonly int _itemId;
+
+        public InternalTarget(FirefliesDeed _deed, int itemId) : base(-1, true, TargetFlags.None)
         {
-            LootType = LootType.Blessed;
-            Weight = 1.0;
+            _firefliesDeed = _deed;
+            _itemId = itemId;
         }
 
-        public override int LabelNumber => 1150061;
-
-        public override void OnDoubleClick(Mobile from)
+        protected override void OnTarget(Mobile from, object targeted)
         {
-            if (IsChildOf(from.Backpack))
+            if (_firefliesDeed?.Deleted != false)
             {
-                var house = BaseHouse.FindHouseAt(from);
-
-                if (house?.IsOwner(from) == true)
-                {
-                    from.CloseGump<FacingGump>();
-
-                    if (!from.SendGump(new FacingGump(this, from)))
-                    {
-                        from.SendLocalizedMessage(1150062); // You fail to re-deed the holiday fireflies.
-                    }
-                }
-                else
-                {
-                    from.SendLocalizedMessage(502092); // You must be in your house to do this.
-                }
+                return;
             }
-            else
+
+            if (!_firefliesDeed.IsChildOf(from.Backpack))
             {
                 from.SendLocalizedMessage(1042038); // You must have the object in your backpack to use it.
-            }
-        }
-
-        private class FacingGump : Gump
-        {
-            private readonly FirefliesDeed m_Deed;
-            private readonly Mobile m_Placer;
-
-            public FacingGump(FirefliesDeed deed, Mobile player)
-                : base(150, 50)
-            {
-                m_Deed = deed;
-                m_Placer = player;
-
-                Closable = true;
-                Disposable = true;
-                Draggable = true;
-                Resizable = false;
-
-                AddPage(0);
-
-                AddBackground(0, 0, 300, 150, 0xA28);
-
-                AddItem(90, 30, 0x2332);
-                AddItem(180, 30, 0x2336);
-
-                AddButton(50, 35, 0x868, 0x869, (int)Buttons.East);
-                AddButton(145, 35, 0x868, 0x869, (int)Buttons.South);
+                return;
             }
 
-            public override void OnResponse(NetState sender, in RelayInfo info)
-            {
-                int m_ItemID;
+            var house = BaseHouse.FindHouseAt(from);
 
-                switch (info.ButtonID)
+            if (house?.IsOwner(from) != true)
+            {
+                from.SendLocalizedMessage(502092); // You must be in your house to do this.
+                return;
+            }
+
+            var p = targeted as IPoint3D;
+            var map = from.Map;
+
+            if (p == null || map == null || map == Map.Internal)
+            {
+                return;
+            }
+
+            var p3d = new Point3D(p);
+            var id = TileData.ItemTable[_itemId & TileData.MaxItemValue];
+
+            if (!map.CanFit(p3d, id.Height))
+            {
+                from.SendLocalizedMessage(500269); // You cannot build that there.
+                return;
+            }
+
+            house = BaseHouse.FindHouseAt(p3d, map, id.Height);
+
+            if (house?.IsOwner(from) != true)
+            {
+                from.SendLocalizedMessage(1042036); // That location is not in your house.
+                return;
+            }
+
+            var north = BaseAddon.IsWall(p3d.X, p3d.Y - 1, p3d.Z, map);
+            var west = BaseAddon.IsWall(p3d.X - 1, p3d.Y, p3d.Z, map);
+
+            if ((_itemId != 0x2336 || !north) && (_itemId != 0x2332 || !west))
+            {
+                from.SendLocalizedMessage(1150065); // Holiday fireflies must be placed next to a wall.
+                return;
+            }
+
+            foreach (Fireflies fireflies in Map.Malas.GetItemsAt<Fireflies>(p3d))
+            {
+                if (fireflies.Z == p3d.Z)
                 {
-                    case (int)Buttons.East:
-                        m_ItemID = 0x2332;
-                        break;
-                    case (int)Buttons.South:
-                        m_ItemID = 0x2336;
-                        break;
-                    default: return;
-                }
-
-                m_Placer.Target = new InternalTarget(m_Deed, m_ItemID);
-            }
-
-            private enum Buttons
-            {
-                Cancel,
-                South,
-                East
-            }
-        }
-
-        private class InternalTarget : Target
-        {
-            private readonly FirefliesDeed m_FirefliesDeed;
-            private readonly int m_ItemID;
-
-            public InternalTarget(FirefliesDeed m_Deed, int itemid)
-                : base(-1, true, TargetFlags.None)
-            {
-                m_FirefliesDeed = m_Deed;
-                m_ItemID = itemid;
-            }
-
-            protected override void OnTarget(Mobile from, object targeted)
-            {
-                if (m_FirefliesDeed?.Deleted != false)
-                {
+                    from.SendLocalizedMessage(1150065); // Holiday fireflies must be placed next to a wall.
                     return;
                 }
-
-                if (m_FirefliesDeed.IsChildOf(from.Backpack))
-                {
-                    var house = BaseHouse.FindHouseAt(from);
-
-                    if (house?.IsOwner(from) == true)
-                    {
-                        var p = targeted as IPoint3D;
-                        var map = from.Map;
-
-                        if (p == null || map == null || map == Map.Internal)
-                        {
-                            return;
-                        }
-
-                        var p3d = new Point3D(p);
-                        var id = TileData.ItemTable[m_ItemID & TileData.MaxItemValue];
-
-                        if (map.CanFit(p3d, id.Height))
-                        {
-                            house = BaseHouse.FindHouseAt(p3d, map, id.Height);
-
-                            if (house?.IsOwner(from) == true)
-                            {
-                                var north = BaseAddon.IsWall(p3d.X, p3d.Y - 1, p3d.Z, map);
-                                var west = BaseAddon.IsWall(p3d.X - 1, p3d.Y, p3d.Z, map);
-
-                                bool isclear = true;
-
-                                foreach (Fireflies fireflies in Map.Malas.GetItemsAt<Fireflies>(p3d))
-                                {
-                                    if (fireflies.Z == p3d.Z)
-                                    {
-                                        isclear = false;
-                                    }
-                                }
-
-                                if ((m_ItemID == 0x2336 && north || m_ItemID == 0x2332 && west) && isclear)
-                                {
-                                    var flies = new Fireflies(m_ItemID);
-
-                                    house.Addons.Add(flies);
-
-                                    flies.MoveToWorld(p3d, from.Map);
-
-                                    m_FirefliesDeed.Delete();
-                                }
-                                else
-                                {
-                                    from.SendLocalizedMessage(1150065); // Holiday fireflies must be placed next to a wall.
-                                }
-                            }
-                            else
-                            {
-                                from.SendLocalizedMessage(1042036); // That location is not in your house.
-                            }
-                        }
-                        else
-                        {
-                            from.SendLocalizedMessage(500269); // You cannot build that there.
-                        }
-                    }
-                    else
-                    {
-                        from.SendLocalizedMessage(502092); // You must be in your house to do this.
-                    }
-                }
-                else
-                {
-                    from.SendLocalizedMessage(1042038); // You must have the object in your backpack to use it.
-                }
             }
+
+            var flies = new Fireflies(_itemId);
+            house.Addons.Add(flies);
+            flies.MoveToWorld(p3d, from.Map);
+            _firefliesDeed.Delete();
         }
     }
 }
