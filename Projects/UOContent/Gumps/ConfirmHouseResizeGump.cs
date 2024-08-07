82ea01ec4613e5c283241feb25e2f27cff805dd1
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-08-07 16:52:35 -0700

    Cleanup

--- Projects/UOContent/Gumps/ConfirmHouseResizeGump.cs
+++ Projects/UOContent/Gumps/ConfirmHouseResizeGump.cs
@@ -1,5 +1,6 @@
 using Server.Guilds;
 using Server.Items;
+using Server.Mobiles;
 using Server.Multis;
 using Server.Network;
 
@@ -9,10 +10,7 @@ public class ConfirmHouseResizeGump : StaticGump<ConfirmHouseResizeGump>
 {
     private readonly BaseHouse _house;
 
-    public ConfirmHouseResizeGump(BaseHouse house) : base(110, 100)
-    {
-        _house = house;
-    }
+    public ConfirmHouseResizeGump(BaseHouse house) : base(110, 100) => _house = house;
 
     protected override void BuildLayout(ref StaticGumpBuilder builder)
     {
@@ -27,14 +25,16 @@ public class ConfirmHouseResizeGump : StaticGump<ConfirmHouseResizeGump>
         builder.AddImageTiled(10, 40, 400, 200, 0xA40);
         builder.AddAlphaRegion(10, 40, 400, 200);
 
-        /* You are attempting to resize your house. You will be refunded the house's
-        value directly to your bank box. All items in the house will *remain behind*
-        and can be *freely picked up by anyone*. Once the house is demolished, however,
-        only this account will be able to place on the land for one hour. This *will*
-        circumvent the normal 7-day waiting period (if it applies to you). This action
-        will not un-condemn any other houses on your account. If you have other,
-        grandfathered houses, this action *WILL* condemn them. Are you sure you wish
-        to continue?*/
+        /*
+         * You are attempting to resize your house. You will be refunded the house's
+         * value directly to your bank box. All items in the house will *remain behind*
+         * and can be *freely picked up by anyone*. Once the house is demolished, however,
+         * only this account will be able to place on the land for one hour. This *will*
+         * circumvent the normal 7-day waiting period (if it applies to you). This action
+         * will not un-condemn any other houses on your account. If you have other,
+         * grandfathered houses, this action *WILL* condemn them. Are you sure you wish
+         * to continue?
+        */
         builder.AddHtmlLocalized(10, 40, 400, 200, 1080196, 0x7F00, false, true);
 
         builder.AddImageTiled(10, 250, 400, 20, 0xA40);
@@ -52,121 +52,88 @@ public class ConfirmHouseResizeGump : StaticGump<ConfirmHouseResizeGump>
             return;
         }
 
-        if (_house.IsOwner(state.Mobile))
-        {
-            if (_house.MovingCrate != null || _house.InternalizedVendors.Count > 0)
-            {
-                state.Mobile.SendLocalizedMessage(
-                    1080455
-                ); // You can not resize your house at this time. Please remove all items fom the moving crate and try again.
-                return;
-            }
-
-            if (!Guild.NewGuildSystem && _house.FindGuildstone() != null)
-            {
-                state.Mobile.SendLocalizedMessage(501389); // You cannot redeed a house with a guildstone inside.
-                return;
-            }
-
-            /*else if (_house.PlayerVendors.Count > 0)
-            {
-              state.Mobile.SendLocalizedMessage( 503236 ); // You need to collect your vendor's belongings before moving.
-              return;
-            }*/
-            if (_house.HasRentedVendors && _house.VendorInventories.Count > 0)
-            {
-                state.Mobile.SendLocalizedMessage(
-                    1062679
-                ); // You cannot do that that while you still have contract vendors or unclaimed contract vendor inventory in your house.
-                return;
-            }
-
-            if (_house.HasRentedVendors)
-            {
-                state.Mobile.SendLocalizedMessage(
-                    1062680
-                ); // You cannot do that that while you still have contract vendors in your house.
-                return;
-            }
-
-            if (_house.VendorInventories.Count > 0)
-            {
-                state.Mobile.SendLocalizedMessage(
-                    1062681
-                ); // You cannot do that that while you still have unclaimed contract vendor inventory in your house.
-                return;
-            }
-
-            if (state.Mobile.AccessLevel >= AccessLevel.GameMaster)
-            {
-                state.Mobile.SendMessage("You do not get a refund for your house as you are not a player");
-                _house.RemoveKeys(state.Mobile);
-                new TempNoHousingRegion(_house, state.Mobile);
-                _house.Delete();
-            }
-            else
-            {
-                Item toGive;
-
-                if (_house.IsAosRules)
-                {
-                    if (_house.Price > 0)
-                    {
-                        toGive = new BankCheck(_house.Price);
-                    }
-                    else
-                    {
-                        toGive = _house.GetDeed();
-                    }
-                }
-                else
-                {
-                    toGive = _house.GetDeed();
-
-                    if (toGive == null && _house.Price > 0)
-                    {
-                        toGive = new BankCheck(_house.Price);
-                    }
-                }
-
-                if (toGive != null)
-                {
-                    var box = state.Mobile.BankBox;
-
-                    if (box.TryDropItem(state.Mobile, toGive, false))
-                    {
-                        if (toGive is BankCheck check)
-                        {
-                            state.Mobile.SendLocalizedMessage(
-                                1060397,
-                                check.Worth.ToString()
-                            ); // ~1_AMOUNT~ gold has been deposited into your bank box.
-                        }
-
-                        _house.RemoveKeys(state.Mobile);
-                        new TempNoHousingRegion(_house, state.Mobile);
-                        _house.Delete();
-                    }
-                    else
-                    {
-                        toGive.Delete();
-                        state.Mobile.SendLocalizedMessage(500390); // Your bank box is full.
-                    }
-                }
-                else
-                {
-                    state.Mobile.SendMessage("Unable to refund house.");
-                }
-            }
-        }
-        else
-        {
-            state.Mobile.SendLocalizedMessage(501320); // Only the house owner may do this.
-        }
+        var from = state.Mobile;
 
         if (info.ButtonID == 0)
         {
-            state.Mobile.SendGump(new HouseGumpAOS(HouseGumpPageAOS.Customize, state.Mobile, _house));
+            from.SendGump(new HouseGumpAOS(HouseGumpPageAOS.Customize, from, _house));
+            return;
         }
+
+        if (!_house.IsOwner(from))
+        {
+            from.SendLocalizedMessage(501320); // Only the house owner may do this.
+            return;
+        }
+
+        if (_house.MovingCrate != null || _house.InternalizedVendors.Count > 0)
+        {
+            // You can not resize your house at this time. Please remove all items fom the moving crate and try again.
+            from.SendLocalizedMessage(1080455);
+            return;
+        }
+
+        if (!Guild.NewGuildSystem && _house.FindGuildstone() != null)
+        {
+            from.SendLocalizedMessage(501389); // You cannot redeed a house with a guildstone inside.
+            return;
+        }
+
+        /*
+        if (_house.PlayerVendors.Count > 0)
+        {
+            from.SendLocalizedMessage(503236); // You need to collect your vendor's belongings before moving.
+            return;
+        }
+        */
+        if (_house.HasRentedVendors && _house.VendorInventories.Count > 0)
+        {
+            // You cannot do that that while you still have contract vendors or unclaimed contract vendor inventory in your house.
+            from.SendLocalizedMessage(1062679);
+            return;
+        }
+
+        if (_house.HasRentedVendors)
+        {
+            // You cannot do that that while you still have contract vendors in your house.
+            from.SendLocalizedMessage(1062680);
+            return;
+        }
+
+        if (_house.VendorInventories.Count > 0)
+        {
+            // You cannot do that that while you still have unclaimed contract vendor inventory in your house.
+            from.SendLocalizedMessage(1062681);
+            return;
+        }
+
+        if (from.AccessLevel > AccessLevel.Player)
+        {
+            from.SendMessage("You do not get a refund for your house as you are not a player.");
+        }
+        else
+        {
+            var toGive = !_house.IsAosRules || _house.Price <= 0 ? _house.GetDeed() : null;
+
+            if (toGive != null && !from.BankBox.TryDropItem(from, toGive, false))
+            {
+                toGive.Delete();
+                from.SendLocalizedMessage(500390); // Your bank box is full.
+                return;
+            }
+
+            if (_house.Price <= 0 || !Banker.Deposit(from, _house.Price))
+            {
+                from.SendMessage("Unable to refund house.");
+                return;
+            }
+
+            // ~1_AMOUNT~ gold has been deposited into your bank box.
+            from.SendLocalizedMessage(1060397, _house.Price.ToString());
+        }
+
+        _house.RemoveKeys(from);
+        new TempNoHousingRegion(_house, from);
+        _house.Delete();
     }
 }
