b754c6454892ba1be1060ec205de692b810c6f71
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-08-10 09:56:40 -0700

    Removes using

--- Projects/UOContent/Gumps/Base/GumpSystem.cs
+++ Projects/UOContent/Gumps/Base/GumpSystem.cs
@@ -13,7 +13,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  *************************************************************************/
 
-using Server.Logging;
 using Server.Network;
 using System;
 using System.Collections.Generic;
