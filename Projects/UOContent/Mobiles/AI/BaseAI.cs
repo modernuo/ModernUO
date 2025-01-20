77fe34ca8695c9a0cdba011b377671dc1b60d4fd
Bohica <ab3.cisco@gmail.com>
2025-01-20 15:50:27 -0800

    Refactor IsInvalidUnfriendRequest method

--- Projects/UOContent/Mobiles/AI/BaseAI.cs
+++ Projects/UOContent/Mobiles/AI/BaseAI.cs
@@ -1383,8 +1383,10 @@ public abstract class BaseAI
         return true;
     }
     
-    private bool IsInvalidUnfriendRequest(Mobile from, Mobile to) =>
-        from?.Deleted != false || to?.Deleted != false || from == to || !to.Player;
+    private bool IsInvalidUnfriendRequest(Mobile from, Mobile to)
+    {
+        return from?.Deleted != false || to?.Deleted != false || from == to || !to.Player;
+    }
     
     private void HandleUnfriendRequest(Mobile from, Mobile to)
     {
