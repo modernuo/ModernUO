de12f8cb02f9e8939a4ffd9bfe46cb26701ae4ed
Bohica <53943479+Bohicatv@users.noreply.github.com>
2025-02-03 17:38:07 -0800

    fix: Fixes turning to target on spellcast (#2107)

--- Projects/UOContent/Spells/Base/SpellHelper.cs
+++ Projects/UOContent/Spells/Base/SpellHelper.cs
@@ -178,21 +178,26 @@ namespace Server.Spells
 
         public static void Turn(Mobile from, IPoint3D to)
         {
-            if (from == null)
+            if (from == null || to == null || from.Equals(to))
             {
                 return;
             }
 
-            var root = (to as Item)?.RootParent;
-            if (from != root)
+            if (to is Item item)
             {
-                to = root;
+                var root = item.RootParent;
+                if (root != null)
+                {
+                    if (from == root)
+                    {
+                        return;
+                    }
+
+                    to = root;
+                }
             }
 
-            if (!from.Equals(to))
-            {
-                from.Direction = from.GetDirectionTo(to);
-            }
+            from.Direction = from.GetDirectionTo(to);
         }
 
         public static bool CheckCombat(Mobile m)
