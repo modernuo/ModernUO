47da961a376eea57b21921dfe47b3076d76c039e
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2025-03-12 13:08:14 -0700

    fix: Fixes spawning guards. Adds back non-lethal with config (#2140)

--- Projects/UOContent/Mobiles/Guards/WarriorGuard.cs
+++ Projects/UOContent/Mobiles/Guards/WarriorGuard.cs
@@ -1,4 +1,3 @@
-using System;
 using ModernUO.Serialization;
 using Server.Items;
 
@@ -7,17 +6,11 @@ namespace Server.Mobiles;
 [SerializationGenerator(0, false)]
 public partial class WarriorGuard : BaseGuard
 {
-    private Timer _attackTimer;
-    private Timer _idleTimer;
-
     [Constructible]
     public WarriorGuard(Mobile target = null) : base(target)
     {
-        InitStats(1000, 1000, 1000);
-        Title = "the guard";
-
+        InitStats(100, 125, 25);
         SpeechHue = Utility.RandomDyedHue();
-
         Hue = Race.Human.RandomSkinHue();
 
         if (Female = Utility.RandomBool())
@@ -73,12 +66,12 @@ public partial class WarriorGuard : BaseGuard
 
         AddItem(weapon);
 
-        Container pack = new Backpack();
-
-        pack.Movable = false;
+        var pack = new Backpack
+        {
+            Movable = false
+        };
 
         pack.DropItem(new Gold(10, 25));
-
         AddItem(pack);
 
         Skills.Anatomy.Base = 120.0;
@@ -88,7 +81,6 @@ public partial class WarriorGuard : BaseGuard
         Skills.DetectHidden.Base = 100.0;
 
         NextCombatTime = Core.TickCount + 500;
-        Focus = target;
     }
 
     [SerializableProperty(0)]
@@ -126,34 +118,22 @@ public partial class WarriorGuard : BaseGuard
                     Say(500131); // Thou wilt regret thine actions, swine!
                 }
 
-                if (_attackTimer != null)
-                {
-                    _attackTimer.Stop();
-                    _attackTimer = null;
-                }
-
-                if (_idleTimer != null)
-                {
-                    _idleTimer.Stop();
-                    _idleTimer = null;
-                }
+                AttackTimer = null;
+                IdleTimer = null;
 
                 if (_focus != null)
                 {
-                    _attackTimer = new AttackTimer(this);
-                    _attackTimer.Start();
-                    ((AttackTimer)_attackTimer).DoOnTick();
+                    AttackTimer = new GuardAttackTimer(this);
+                    AttackTimer.DoOnTick();
                 }
                 else
                 {
-                    _idleTimer = new IdleTimer(this);
-                    _idleTimer.Start();
+                    IdleTimer = new GuardIdleTimer(this);
                 }
             }
-            else if (_focus == null && _idleTimer == null)
+            else if (_focus == null && IdleTimer == null && Spawner != null)
             {
-                _idleTimer = new IdleTimer(this);
-                _idleTimer.Start();
+                IdleTimer = new GuardIdleTimer(this);
             }
 
             this.MarkDirty();
@@ -164,209 +144,32 @@ public partial class WarriorGuard : BaseGuard
     {
         if (_focus?.Alive == true)
         {
-            new AvengeTimer(_focus).Start(); // If a guard dies, three more guards will spawn
+            new GuardAvengeTimer(_focus).Start(); // If a guard dies, three more guards will spawn
         }
 
         return base.OnBeforeDeath();
     }
 
-    [AfterDeserialization]
-    private void AfterDeserialization()
+    public override void NonLethalAttack(Mobile target)
     {
-        if (_focus != null)
+        if (!InRange(target, 20))
         {
-            _attackTimer = new AttackTimer(this);
-            _attackTimer.Start();
+            Focus = null;
         }
-        else
+        else if (!InRange(target, 10) || !InLOS(target))
         {
-            _idleTimer = new IdleTimer(this);
-            _idleTimer.Start();
+            TeleportTo(this, target.Location);
         }
-    }
-
-    public override void OnAfterDelete()
-    {
-        if (_attackTimer != null)
+        else if (!InRange(target, 1))
         {
-            _attackTimer.Stop();
-            _attackTimer = null;
+            if (!Move(GetDirectionTo(target) | Direction.Running))
+            {
+                TeleportTo(this, target.Location);
+            }
         }
-
-        if (_idleTimer != null)
+        else if (!CanSee(target) && UseSkill(SkillName.DetectHidden))
         {
-            _idleTimer.Stop();
-            _idleTimer = null;
-        }
-
-        base.OnAfterDelete();
-    }
-
-    private class AvengeTimer : Timer
-    {
-        private readonly Mobile m_Focus;
-
-        public AvengeTimer(Mobile focus) : base(TimeSpan.FromSeconds(2.5), TimeSpan.FromSeconds(1.0), 3) =>
-            m_Focus = focus;
-
-        protected override void OnTick()
-        {
-            Spawn(m_Focus, m_Focus, 1, true);
-        }
-    }
-
-    private class AttackTimer : Timer
-    {
-        private readonly WarriorGuard m_Owner;
-
-        public AttackTimer(WarriorGuard owner) : base(TimeSpan.FromSeconds(0.25), TimeSpan.FromSeconds(0.1)) =>
-            m_Owner = owner;
-
-        public void DoOnTick()
-        {
-            OnTick();
-        }
-
-        protected override void OnTick()
-        {
-            if (m_Owner.Deleted)
-            {
-                Stop();
-                return;
-            }
-
-            m_Owner.Criminal = false;
-            m_Owner.Kills = 0;
-            m_Owner.Stam = m_Owner.StamMax;
-
-            var target = m_Owner.Focus;
-
-            if (target != null && (target.Deleted || !target.Alive || !m_Owner.CanBeHarmful(target)))
-            {
-                m_Owner.Focus = null;
-                Stop();
-                return;
-            }
-
-            if (m_Owner.Weapon is Fists)
-            {
-                m_Owner.Kill();
-                Stop();
-                return;
-            }
-
-            if (target != null && m_Owner.Combatant != target)
-            {
-                m_Owner.Combatant = target;
-            }
-
-            if (target == null)
-            {
-                Stop();
-            }
-            else
-            {
-                // <instakill>
-                TeleportTo(target);
-                target.BoltEffect(0);
-
-                if (target is BaseCreature creature)
-                {
-                    creature.NoKillAwards = true;
-                }
-
-                target.Damage(target.HitsMax, m_Owner);
-                target.Kill(); // just in case, maybe Damage is overridden on some shard
-
-                if (target.Corpse != null && !target.Player)
-                {
-                    target.Corpse.Delete();
-                }
-
-                m_Owner.Focus = null;
-                Stop();
-            } // </instakill>
-
-            /*else if (!m_Owner.InRange( target, 20 ))
-            {
-              m_Owner.Focus = null;
-            }
-            else if (!m_Owner.InRange( target, 10 ) || !m_Owner.InLOS( target ))
-            {
-              TeleportTo( target );
-            }
-            else if (!m_Owner.InRange( target, 1 ))
-            {
-              if (!m_Owner.Move( m_Owner.GetDirectionTo( target ) | Direction.Running ))
-                TeleportTo( target );
-            }
-            else if (!m_Owner.CanSee( target ))
-            {
-              if (!m_Owner.UseSkill( SkillName.DetectHidden ) && Utility.Random( 50 ) == 0)
-                m_Owner.Say( "Reveal!" );
-            }*/
-        }
-
-        private void TeleportTo(Mobile target)
-        {
-            var from = m_Owner.Location;
-            var to = target.Location;
-
-            m_Owner.Location = to;
-
-            Effects.SendLocationParticles(
-                EffectItem.Create(from, m_Owner.Map, EffectItem.DefaultDuration),
-                0x3728,
-                10,
-                10,
-                2023
-            );
-            Effects.SendLocationParticles(
-                EffectItem.Create(to, m_Owner.Map, EffectItem.DefaultDuration),
-                0x3728,
-                10,
-                10,
-                5023
-            );
-
-            m_Owner.PlaySound(0x1FE);
-        }
-    }
-
-    private class IdleTimer : Timer
-    {
-        private readonly WarriorGuard m_Owner;
-        private int m_Stage;
-
-        public IdleTimer(WarriorGuard owner) : base(TimeSpan.FromSeconds(2.0), TimeSpan.FromSeconds(2.5)) =>
-            m_Owner = owner;
-
-        protected override void OnTick()
-        {
-            if (m_Owner.Deleted)
-            {
-                Stop();
-                return;
-            }
-
-            if (m_Stage++ % 4 == 0 || !m_Owner.Move(m_Owner.Direction))
-            {
-                m_Owner.Direction = (Direction)Utility.Random(8);
-            }
-
-            if (m_Stage > 16)
-            {
-                Effects.SendLocationParticles(
-                    EffectItem.Create(m_Owner.Location, m_Owner.Map, EffectItem.DefaultDuration),
-                    0x3728,
-                    10,
-                    10,
-                    2023
-                );
-                m_Owner.PlaySound(0x1FE);
-
-                m_Owner.Delete();
-            }
+            Say("Reveal!");
         }
     }
 }
