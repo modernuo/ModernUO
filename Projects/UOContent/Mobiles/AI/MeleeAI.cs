f61e1f3ab153638223ace99b5b5d1f020d6ee65c
Bohica <53943479+Bohicatv@users.noreply.github.com>
2025-05-17 09:04:17 -0700

    Fixes speed when exiting out of flee action

--- Projects/UOContent/Mobiles/AI/MeleeAI.cs
+++ Projects/UOContent/Mobiles/AI/MeleeAI.cs
@@ -179,6 +179,8 @@ public class MeleeAI : BaseAI
                 m_Mobile.DebugSay("I am stronger now, so I will continue fighting");
             }
 
+            m_Mobile.PlaySound(m_Mobile.GetAttackSound());
+            m_Mobile.CurrentSpeed = m_Mobile.ActiveSpeed;
             Action = ActionType.Combat;
         }
         else
