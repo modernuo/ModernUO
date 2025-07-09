ddee9178499312650350200cde51e8e00c4659a1
Bohica <ab3.cisco@gmail.com>
2025-07-09 08:59:25 -0700

    cleanup

--- Projects/UOContent/Mobiles/AI/BaseAI/AIType.cs
+++ Projects/UOContent/Mobiles/AI/BaseAI/AIType.cs
@@ -2,7 +2,7 @@
  * ModernUO                                                              *
  * Copyright 2019-2025 - ModernUO Development Team                       *
  * Email: hi@modernuo.com                                                *
- * File: AIType.cs                                                      *
+ * File: AIType.cs                                                       *
  *                                                                       *
  * This program is free software: you can redistribute it and/or modify  *
  * it under the terms of the GNU General Public License as published by  *
