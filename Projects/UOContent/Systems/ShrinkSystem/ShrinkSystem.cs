7e3221cd1d2bd50eef3927c14c2e0fae63fac216
Bohica <53943479+Bohicatv@users.noreply.github.com>
2025-10-07 18:10:37 -0700

    feat: Adds a shrinking system for pet management
    Implements a shrinking system for shrinking and restoring pets.

---
+++ Projects/UOContent/Systems/ShrinkSystem/ShrinkSystem.cs
@@ -0,0 +1,142 @@
+using System;
+using Server;
+using Server.Items;
+using Server.Mobiles;
+using Server.Targeting;
+
+namespace Server.ShrinkSystem
+{
+     public class ShrinkSystem
+     {
+          public static void Initialize()
+          {    // command = [shrink
+               // change access level here (premium setup)
+               CommandSystem.Register("shrink", AccessLevel.Player, new CommandEventHandler(Shrink_OnCommand));
+          }
+
+          [Usage("shrink")] // target again or double click to restore
+          [Description("Target your pet to shrink it.")]
+          private static void Shrink_OnCommand(CommandEventArgs e)
+          {
+               e.Mobile.SendMessage("Target your pet to shrink.");
+               e.Mobile.Target = new ShrinkToggleTarget();
+          }
+
+          private class ShrinkToggleTarget : Target
+          {
+               public ShrinkToggleTarget() : base(5, false, TargetFlags.None)
+               { 
+               }
+
+               protected override void OnTarget(Mobile from, object targeted)
+               {
+                    if (targeted is BaseCreature pet)
+                    {
+                         if (!pet.Controlled || pet.ControlMaster != from)
+                         {
+                              from.SendMessage("That is not your pet.");
+                              return;
+                         }
+
+                         if (pet.IsDeadPet)
+                         {
+                              from.SendMessage("You cannot shrink a dead pet.");
+                              return;
+                         }
+
+                         if (pet.Combatant != null)
+                         {
+                              from.SendMessage("You cannot shrink a pet while it is in combat.");
+                              return;
+                         }
+
+                         var statuette = new ShrinkBox(pet);
+                         if (from.Backpack != null && from.Backpack.TryDropItem(from, statuette, false))
+                         {
+                              from.SendMessage("Your pet was shrunken to a box in your backpack.");
+                              pet.Delete();
+                         }
+                         else
+                         {
+                              statuette.Delete();
+                              from.SendMessage("You do not have enough space in your backpack.");
+                         }
+                    }
+                    else if (targeted is ShrinkBox statuette)
+                    {
+                         statuette.ReleasePet(from);
+                    }
+                    else
+                    {
+                         from.SendMessage("That is not a valid shruken pet box.");
+                    }
+               }
+          }
+     }
+
+     [Flippable(0x09A8, 0x0E80)]
+     public class ShrinkBox : Item
+     {
+          private string _PetType;
+          private string _PetName;
+          private int _PetHue;
+
+          public ShrinkBox(BaseCreature pet) : base(0x09A8)
+          {
+               Name = $"a shrunken {pet.Name}";
+               _PetType = pet.GetType().FullName;
+               _PetName = pet.Name;
+               _PetHue = pet.Hue;
+               Weight = 1.0;
+               Hue = pet.Hue;
+          }
+
+          public ShrinkBox(Serial serial) : base(serial)
+          {
+          }
+
+          public void ReleasePet(Mobile from)
+          {
+               if (!IsChildOf(from.Backpack))
+               {
+                    from.SendMessage("That must be in your backpack to use.");
+                    return;
+               }
+
+               Type petType = Type.GetType(_PetType);
+               if (petType == null)
+               {
+                    from.SendMessage("This pet type cannot be restored.");
+                    return;
+               }
+
+               if (Activator.CreateInstance(petType) is BaseCreature pet)
+               {
+                    pet.Controlled = true;
+                    pet.ControlMaster = from;
+                    pet.Name = _PetName;
+                    pet.Hue = _PetHue;
+                    pet.MoveToWorld(from.Location, from.Map);
+                    from.SendMessage("Your pet has been restored.");
+                    Delete();
+               }
+               else
+               {
+                    from.SendMessage("Failed to restore pet.");
+               }
+          }
+
+          public override void OnDoubleClick(Mobile from)
+          {
+               if (!IsChildOf(from.Backpack))
+               {
+                    from.SendMessage("That must be in your backpack to use.");
+                    return;
+               }
+               else
+               {
+                    ReleasePet(from);
+               }
+          }
+     }
+}
