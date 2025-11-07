1865c2e9e42097951ca46a59c21421cc44e10aed
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2025-11-06 17:21:44 -0800

    fix: Cleans up legacy gump. Adds size/style support to gump builders. Optimizes EscapeHtml

--- Projects/UOContent/Gumps/Base/DynamicGumpBuilder.cs
+++ Projects/UOContent/Gumps/Base/DynamicGumpBuilder.cs
@@ -134,6 +134,76 @@ public ref struct DynamicGumpBuilder
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtml(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        ReadOnlySpan<char> text,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        var handler = text.Color(color, size);
+        AddHtml(x, y, width, height, ref handler, background, scrollbar);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtml(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        ref RawInterpolatedStringHandler handler,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        AddHtml(x, y, width, height, color, size, handler.Text, background, scrollbar);
+        handler.Clear();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtml(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        byte fontStyle,
+        ReadOnlySpan<char> text,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        var handler = text.Color(color, size, fontStyle);
+        AddHtml(x, y, width, height, ref handler, background, scrollbar);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtml(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        byte fontStyle,
+        ref RawInterpolatedStringHandler handler,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        AddHtml(x, y, width, height, color, size, fontStyle, handler.Text, background, scrollbar);
+        handler.Clear();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void AddHtmlCentered(
         int x, int y, int width, int height, ReadOnlySpan<char> text, bool background = false, bool scrollbar = false
     )
@@ -186,6 +256,76 @@ public ref struct DynamicGumpBuilder
     )
     {
         AddHtmlCentered(x, y, width, height, color, handler.Text, background, scrollbar);
+        handler.Clear();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtmlCentered(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        ReadOnlySpan<char> text,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        var handler = text.Center(color, size);
+        AddHtml(x, y, width, height, ref handler, background, scrollbar);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtmlCentered(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        ref RawInterpolatedStringHandler handler,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        AddHtmlCentered(x, y, width, height, color, size, handler.Text, background, scrollbar);
+        handler.Clear();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtmlCentered(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        byte fontStyle,
+        ReadOnlySpan<char> text,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        var handler = text.Center(color, size, fontStyle);
+        AddHtml(x, y, width, height, ref handler, background, scrollbar);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddHtmlCentered(
+        int x,
+        int y,
+        int width,
+        int height,
+        int color,
+        int size,
+        byte fontStyle,
+        ref RawInterpolatedStringHandler handler,
+        bool background = false,
+        bool scrollbar = false
+    )
+    {
+        AddHtmlCentered(x, y, width, height, color, size, fontStyle, handler.Text, background, scrollbar);
         handler.Clear();
     }
 
