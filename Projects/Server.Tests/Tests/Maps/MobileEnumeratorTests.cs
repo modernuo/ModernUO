88f9a99f87a1704ce010101727e914d4b3711db3
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2025-11-25 17:36:30 -0800

    WIP Tests

---
+++ Projects/Server.Tests/Tests/Maps/MobileEnumeratorTests.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using Server.Collections;
+using Xunit;
+
+namespace Server.Tests.Tests.Maps;
+
+[Collection("Sequential Server Tests")]
+public class MobileEnumeratorTests
+{
+    [Fact]
+    public void MobileEnumerator_FiltersByBoundsAndOrder()
+    {
+        var map = Map.Felucca;
+        var rect = new Rectangle2D(100, 100, 32, 32);
+
+        var mobiles = new Mobile[3];
+        try
+        {
+            mobiles[0] = CreateMobile(map, new Point3D(105, 105, 0));
+            mobiles[1] = CreateMobile(map, new Point3D(130, 130, 0));
+            mobiles[2] = CreateMobile(map, new Point3D(90, 90, 0));
+
+            var found = new List<Mobile>();
+            using var enumerator = map.GetMobilesInBounds<Mobile>(rect).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                found.Add(enumerator.Current);
+            }
+
+            Assert.Equal(2, found.Count);
+            Assert.All(found, m => Assert.True(rect.Contains(m.Location)));
+            Assert.Equal(new[] { mobiles[0], mobiles[1] }, found);
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    [Fact]
+    public void MobileEnumerator_DeletedMobilesAreSkipped()
+    {
+        var map = Map.Felucca;
+        var rect = new Rectangle2D(200, 200, 16, 16);
+
+        var mobiles = new Mobile[3];
+        try
+        {
+            mobiles[0] = CreateMobile(map, new Point3D(205, 205, 0));
+            mobiles[1] = CreateMobile(map, new Point3D(206, 205, 0));
+            mobiles[2] = CreateMobile(map, new Point3D(207, 205, 0));
+
+            mobiles[1].Delete();
+
+            var found = new List<Mobile>();
+            using var enumerator = map.GetMobilesInBounds<Mobile>(rect).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                found.Add(enumerator.Current);
+            }
+
+            Assert.Equal(new[] { mobiles[0], mobiles[2] }, found);
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    [Fact]
+    public void MobileEnumerator_RespectsMakeBoundsInclusiveFlag()
+    {
+        var map = Map.Felucca;
+        var rect = new Rectangle2D(300, 300, 1, 1);
+
+        var mobiles = new Mobile[1];
+        try
+        {
+            mobiles[0] = CreateMobile(map, new Point3D(301, 301, 0));
+
+            var enumerator = map.GetMobilesInBounds<Mobile>(rect, makeBoundsInclusive: true).GetEnumerator();
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(mobiles[0], enumerator.Current);
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    [Fact]
+    public void MobileEnumerator_MapNullYieldsEmpty()
+    {
+        var enumerator = new Map.MobileEnumerator<Mobile>(null, Rectangle2D.Empty, false);
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Fact]
+    public void MobileEnumerator_ThrowsOnVersionChange()
+    {
+        var map = Map.Felucca;
+        var rect = new Rectangle2D(400, 400, 16, 16);
+
+        var mobiles = new[]
+        {
+            CreateMobile(map, new Point3D(405, 405, 0)),
+            CreateMobile(map, new Point3D(406, 405, 0))
+        };
+
+        try
+        {
+            var enumerator = map.GetMobilesInBounds<Mobile>(rect).GetEnumerator();
+            Assert.True(enumerator.MoveNext());
+
+            mobiles[1].Delete();
+
+            Assert.Throws<InvalidOperationException>(() => enumerator.MoveNext());
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    [Fact]
+    public void MobileEnumerator_StepsAcrossSectors()
+    {
+        var map = Map.Felucca;
+        var rect = new Rectangle2D(500, 500, Map.SectorSize * 2, Map.SectorSize * 2);
+
+        var mobiles = new[]
+        {
+            CreateMobile(map, new Point3D(rect.X + 1, rect.Y + 1, 0)),
+            CreateMobile(map, new Point3D(rect.X + Map.SectorSize + 1, rect.Y + 1, 0)),
+            CreateMobile(map, new Point3D(rect.X + Map.SectorSize + 1, rect.Y + Map.SectorSize + 1, 0))
+        };
+
+        try
+        {
+            var result = new List<Mobile>();
+            using var enumerator = map.GetMobilesInBounds<Mobile>(rect).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                result.Add(enumerator.Current);
+            }
+
+            Assert.Equal(mobiles, result);
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    [Fact]
+    public void MobileEnumerator_MapBoundsAreClamped()
+    {
+        var map = Map.Felucca;
+        var width = map.Width;
+        var height = map.Height;
+
+        var rect = new Rectangle2D(width - Map.SectorSize - 2, height - Map.SectorSize - 2, Map.SectorSize * 2, Map.SectorSize * 2);
+
+        var mobiles = new[]
+        {
+            CreateMobile(map, new Point3D(width - 2, height - 2, 0))
+        };
+
+        try
+        {
+            var enumerator = map.GetMobilesInBounds<Mobile>(rect).GetEnumerator();
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(mobiles[0], enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+        }
+        finally
+        {
+            DeleteAll(mobiles);
+        }
+    }
+
+    private static Mobile CreateMobile(Map map, Point3D location)
+    {
+        var mobile = new Mobile((Serial)Utility.RandomMinMax(0x100, 0xFFF));
+        mobile.DefaultMobileInit();
+        mobile.MoveToWorld(location, map);
+        return mobile;
+    }
+
+    private static void DeleteAll(IReadOnlyList<Mobile> mobiles)
+    {
+        for (var i = 0; i < mobiles.Count; i++)
+        {
+            mobiles[i]?.Delete();
+        }
+    }
+}
