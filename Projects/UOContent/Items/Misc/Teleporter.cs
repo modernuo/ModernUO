ebf96ab9959a5fa9c9aeab432a081c28e2404b95
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2025-11-29 16:27:30 -0800

    fix: Fixes ConditionTeleporter.DenyFollowers

--- Projects/UOContent/Items/Misc/Teleporter.cs
+++ Projects/UOContent/Items/Misc/Teleporter.cs
@@ -866,8 +866,7 @@ public partial class ConditionTeleporter : Teleporter
             return false;
         }
 
-        if (GetFlag(ConditionFlag.DenyFollowers) &&
-            (m.Followers != 0 || m is PlayerMobile mobile && mobile.AutoStabled?.Count != 0))
+        if (GetFlag(ConditionFlag.DenyFollowers) && (m.Followers != 0 || m is PlayerMobile { AutoStabled.Count: > 0 }))
         {
             m.SendLocalizedMessage(1077250); // No pets permitted beyond this point.
             return false;
