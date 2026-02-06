4df4fcc1498102cc445c40ef25cc25ec194e76f0
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2026-02-05 23:29:43 -0800

    feat: Adds Feature Flag System

--- Projects/UOContent/Engines/Bulk Orders/LargeBOD.cs
+++ Projects/UOContent/Engines/Bulk Orders/LargeBOD.cs
@@ -1,6 +1,7 @@
 using ModernUO.Serialization;
 using Server.Gumps;
 using Server.Mobiles;
+using Server.Systems.FeatureFlags;
 
 namespace Server.Engines.BulkOrders
 {
@@ -75,14 +76,19 @@ namespace Server.Engines.BulkOrders
 
         public override void OnDoubleClick(Mobile from)
         {
-            if (IsChildOf(from.Backpack) || InSecureTrade || RootParent is PlayerVendor)
-            {
-                from.SendGump(new LargeBODGump(this));
-            }
-            else
+            if (!(IsChildOf(from.Backpack) || InSecureTrade || RootParent is PlayerVendor))
             {
                 from.SendLocalizedMessage(1045156); // You must have the deed in your backpack to use it.
+                return;
             }
+
+            if (!ContentFeatureFlags.BulkOrders && from.AccessLevel < AccessLevel.Administrator)
+            {
+                from.SendMessage(0x22, "Bulk orders are temporarily disabled.");
+                return;
+            }
+
+            from.SendGump(new LargeBODGump(this));
         }
 
         public override void EndCombine(Mobile from, Item item)
