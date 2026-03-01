3245dd9669d154b4ed4efb6ba040f474857beb82
Bohicatv <ab3.cisco@gmail.com>
2026-02-28 19:10:12 -0800

    fixes fireball projectile animation

--- Projects/UOContent/Spells/Third/Fireball.cs
+++ Projects/UOContent/Spells/Third/Fireball.cs
@@ -20,40 +20,101 @@ namespace Server.Spells.Third
 
         public override bool DelayedDamage => true;
 
-        public void Target(Mobile m)
+        public void Target(Mobile defender)
         {
-            if (CheckHSequence(m))
+            if (CheckHSequence(defender))
             {
-                var source = Caster;
+                var attacker = Caster;
 
-                SpellHelper.Turn(source, m);
+                SpellHelper.Turn(attacker, defender);
 
-                SpellHelper.CheckReflect((int)Circle, ref source, ref m);
+                SpellHelper.CheckReflect((int)Circle, ref attacker, ref defender);
 
                 double damage;
 
                 if (Core.AOS)
                 {
-                    damage = GetNewAosDamage(19, 1, 5, m);
+                    damage = GetNewAosDamage(19, 1, 5, defender);
                 }
                 else
                 {
                     damage = Utility.Random(10, 7);
 
-                    if (CheckResisted(m))
+                    if (CheckResisted(defender))
                     {
                         damage *= 0.75;
 
-                        m.SendLocalizedMessage(501783); // You feel yourself resisting magical energy.
+                        defender.SendLocalizedMessage(501783); // You feel yourself resisting magical energy.
                     }
 
-                    damage *= GetDamageScalar(m);
+                    damage *= GetDamageScalar(defender);
                 }
 
-                source.MovingParticles(m, 0x36D4, 7, 0, false, true, 9502, 4019, 0x160);
-                source.PlaySound(Core.AOS ? 0x15E : 0x44B);
+                int deltaX = defender.X - attacker.X;
+                int deltaY = defender.Y - attacker.Y;
+            
+                int xOffset = 0, yOffset = 0;
+                int zOffset = attacker.Mount != null ? 0 : 0;
+            
+                if (deltaX == 0 && deltaY < 0)
+                {
+                    xOffset = 0;
+                    yOffset = 0;
+                    zOffset = attacker.Mount != null ? 12 : 8;
+                }
+                else if (deltaX > 0 && deltaY < 0)
+                {
+                    xOffset = 1;
+                    yOffset = -1;
+                    zOffset = attacker.Mount != null ? 9 : 4;
+                }
+                else if (deltaX > 0 && deltaY == 0)
+                {
+                    xOffset = 1;
+                    yOffset = 0;
+                    zOffset = attacker.Mount != null ? 9 : 4;
+                }
+                else if (deltaX > 0 && deltaY > 0)
+                {
+                    xOffset = 0;
+                    yOffset = 1;
+                    zOffset = attacker.Mount != null ? 6 : 3;
+                }
+                else if (deltaX == 0 && deltaY > 0)
+                {
+                    xOffset = 0;
+                    yOffset = 1;
+                    zOffset = attacker.Mount != null ? 16 : 8;
+                
+                }
+                else if (deltaX < 0 && deltaY > 0)
+                {
+                    xOffset = -1;
+                    yOffset = 1;
+                    zOffset = attacker.Mount != null ? 16 : 8;
+                }
+                else if (deltaX < 0 && deltaY == 0)
+                {
+                    xOffset = -1;
+                    yOffset = 0;
+                    zOffset = attacker.Mount != null ? 16 : 8;
+                }
+                else if (deltaX < 0 && deltaY < 0)
+                {
+                    xOffset = 0;
+                    yOffset = 0;
+                    zOffset = attacker.Mount != null ? 16 : 8;
+                }
+            
+                Point3D from = new(attacker.X + xOffset, attacker.Y + yOffset, attacker.Z + zOffset);
+                Point3D to = new(defender.X + xOffset, defender.Y + yOffset, defender.Z + zOffset);
+            
+                Effects.SendMovingEffect(new Entity(Serial.Zero, from, attacker.Map),
+                    new Entity(Serial.Zero, to, defender.Map), 0x36D4, 7, 0, false, true);
 
-                SpellHelper.Damage(this, m, damage, 0, 100, 0, 0, 0);
+                attacker.PlaySound(Core.AOS ? 0x15E : 0x44B);
+
+                SpellHelper.Damage(this, defender, damage, 0, 100, 0, 0, 0);
             }
         }
 
